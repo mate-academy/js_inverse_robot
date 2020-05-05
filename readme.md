74f7f773368ea15dd9907922d8382a540061c1ef
Den <37452763+Denchepornyuk@users.noreply.github.com>
2020-05-05 15:11:30 +0300

    Initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Sum() - [put the task name here]
+[put a short description here] Implement the `sum` function returning a sum of two numbers
+
+**Read [the guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md) before start**
---
+++ src/sum.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Implement sum function:
+ *
+ * Function takes 2 numbers and returns their sum
+ *
+ * sum(1, 2) === 3
+ * sum(1, 11) === 12
+ *
+ * @param {number} a
+ * @param {number} b
+ *
+ * @return {number}
+ */
+function sum(a, b) {
+  // write code here
+  return a + b;
+}
+
+module.exports = sum;
