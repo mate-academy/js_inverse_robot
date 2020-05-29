36528f2f9951e9adf74d2468626f1f6a2124bc02
Ihor Hranetskyi <graniecki20@gmail.com>
2020-05-29 22:14:13 +0300

    Finish solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,28 +24,14 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  let newRobot = {};
+  const newRobot = {};
   const values = Object.keys(robot);
   const keys = Object.values(robot);
 
-  for (let x = 0; x < keys.length - 1; x++) {
-    let j = 1;
-
-    if (x > 0) {
-      j += x;
-    }
-
-    for (j; j < keys.length; j++) {
-      if (keys[x] !== keys[j]) {
-        continue;
-      }
-      newRobot = null;
-
-      return newRobot;
-    }
-  }
-
   for (let i = 0; i < keys.length; i++) {
+    if (newRobot.hasOwnProperty(keys[i])) {
+      return null;
+    }
     newRobot[keys[i]] = values[i];
   }
 
