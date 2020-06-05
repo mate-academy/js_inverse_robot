fa3ebce1388ac473e05f42b8e7255e27951a0422
Anatoliy Pokhylko <pokhilko.av@gmail.com>
2020-06-05 16:57:22 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,6 +7,18 @@
  */
 function inverseRobot(robot) {
   // write code here
+
+  const repaireRobot = {};
+
+  for (const detail in robot) {
+    if (repaireRobot.hasOwnProperty(robot[detail])) {
+      return null;
+    }
+
+    repaireRobot[robot[detail]] = detail;
+  }
+
+  return repaireRobot;
 }
 
 module.exports = inverseRobot;
