ec9a18fac4ab76dbb6966887d630688d373bb7f7
Elena-brv <leena.brv@gmail.com>
2020-08-03 20:59:51 +0300

    Revert "Solution"
    This reverts commit 9336d41b0ca6e39ada05f0b9b11cc8b0f87c77cb.

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,16 +24,7 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  const newRobot = {};
-
-  for (const key in robot) {
-    if (newRobot.hasOwnProperty(robot[key])) {
-      return null;
-    }
-    newRobot[robot[key]] = key;
-  }
-
-  return newRobot;
+  // write code here
 }
 
 module.exports = inverseRobot;
