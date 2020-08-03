d2efd8300937c8827b8c630b4a62daaa5f7c9301
Anatasia <krajnova.stasy@gmail.com>
2020-08-03 15:23:09 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reversed = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (reversed[value] !== undefined) {
+      return null;
+    } else {
+      reversed[value] = key;
+    }
+  }
+
+  return reversed;
 }
 
 module.exports = inverseRobot;
