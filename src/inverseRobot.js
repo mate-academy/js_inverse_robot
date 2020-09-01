c22ae3e28439cf31c5c372b94163ebb2fb699517
dmitry <akulinind@gmail.com>
2020-09-01 16:20:07 +0300

    add inverse robot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,23 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const keys = [];
+  const values = [];
+  const inversedRobot = {};
+
+  for (const property in robot) {
+    if (keys.includes(robot[property])) {
+      return null;
+    }
+    keys.push(robot[property]);
+    values.push(property);
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    inversedRobot[keys[i]] = values[i];
+  }
+
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
