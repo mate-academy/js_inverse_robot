fa8d5f97bb4afb2ba8081f4eb9baa57d07ed53a7
Viktor Morhun <victor.morgun16@gmail.com>
2020-12-15 15:48:19 +0200

    redone using hasOwnProperty

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,17 +1,11 @@
 'use strict';
 
 function inverseRobot(robot) {
-  let counter = 1;
-
   for (const key of Object.keys(robot)) {
-    const arr = Object.values(robot);
-
-    for (let i = counter; i < arr.length; i++) {
-      if (robot[key] === arr[i]) {
-        return null;
-      }
+    if (robot.hasOwnProperty(robot[key])) {
+      return null;
     }
-    counter++;
+
     robot[robot[key]] = key;
     delete robot[key];
   }
