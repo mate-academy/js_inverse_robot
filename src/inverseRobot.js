8a0715db5207e2cc51b6ae6534878329aefe626d
Владислав Сандык <nubia@MacBook-Pro-Vladislav.local>
2021-02-10 18:00:42 +0200

    solved

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    const value = robot[key];
+
+    newRobot[value] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
