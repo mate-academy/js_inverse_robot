826311fd9cebf81e8097fa59483dfedb52b1f102
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-06-20 16:42:05 +0300

    task is completed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,6 +25,19 @@
  */
 function inverseRobot(robot) {
   // write code here
+  const newObj = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (newObj.hasOwnProperty(value)) {
+      return null;
+    }
+
+    newObj[value] = key;
+  }
+
+  return newObj;
 }
 
 module.exports = inverseRobot;
