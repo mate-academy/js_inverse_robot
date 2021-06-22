520c389c465132bbf822505ad9a31028ed231c25
Grygoriy Ponomarenko <profit4fam@gmail.com>
2021-06-22 21:32:05 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const fixedRobot = {};
+
+  for (const key in robot) {
+    const newKey = robot[key];
+
+    if (!fixedRobot.hasOwnProperty(newKey)) {
+      fixedRobot[newKey] = key;
+    } else {
+      return null;
+    }
+  }
+
+  return fixedRobot;
 }
 
 module.exports = inverseRobot;
