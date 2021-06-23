b17077c1fe7a1ae24f2e75dfb1914538cddcc160
Stas Pechenyi <spechenuy@gmail.com>
2021-06-23 17:05:41 +0300

    Issues were fixxed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,21 +25,16 @@
  */
 function inverseRobot(robot) {
   const fixxedRobot = {};
-  let fixxedRobotLength = 0;
-  let brokenRobotLenght = 0;
 
   for (const key in robot) {
+    if (fixxedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
     fixxedRobot[robot[key]] = key;
-    brokenRobotLenght++;
   }
 
-  fixxedRobotLength = Object.keys(fixxedRobot).length;
-
-  if (brokenRobotLenght === fixxedRobotLength) {
-    return fixxedRobot;
-  } else {
-    return null;
-  }
+  return fixxedRobot;
 }
 
 module.exports = inverseRobot;
