7e0a44c86ee8a7f8bbb3afcb71d49fdc33ad5a3a
Sasha Lifashkin <alifashkin@outlook.com>
2021-08-02 18:56:43 +0300

    done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const copy = {};
+
+  for (const key in robot) {
+    if (robot[key] in copy) {
+      return null;
+    }
+
+    copy[robot[key]] = key;
+  }
+
+  return copy;
 }
 
 module.exports = inverseRobot;
