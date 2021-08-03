707da3aba665a25c319a0fac576d65e9fb36ac10
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-03 17:12:08 +0300

    Inverse-robot solved

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const normalRobot = {};
+
+  for (const key in robot) {
+    if (normalRobot[robot[key]]) {
+      return null;
+    }
+
+    normalRobot[robot[key]] = key;
+  }
+
+  return normalRobot;
 }
 
 module.exports = inverseRobot;
