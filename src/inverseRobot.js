bcf5a31738998b86d988426e6303fa3b608a59c1
Katerina Bovdyr <katok5673966@gmail.com>
2021-08-26 14:44:01 +0300

    commit-2

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,17 +1,14 @@
 'use strict';
 
 function inverseRobot(robot) {
-  const values = Object.keys(robot);
-  const keys = Object.values(robot);
   const invertRobot = {};
 
-  if (keys.length > 0) {
-    for (let i = 0; i < keys.length; i++) {
-      if (invertRobot.hasOwnProperty(keys[i])) {
-        return null;
-      }
-      invertRobot[ keys[i] ] = values[i];
+  for (const key in robot) {
+    if (invertRobot.hasOwnProperty(robot[key])) {
+      return null;
     }
+
+    invertRobot[robot[key]] = key;
   }
 
   return invertRobot;
