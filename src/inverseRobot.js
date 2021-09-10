e5e3bd7626f324e9b4a83c76d3417c8ae4ac7d7e
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-09-10 12:56:20 +0300

    solution js_inverse_robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reverseObject = {};
+
+  for (const key in robot) {
+    if (robot[key] in reverseObject) {
+      return null;
+    } else {
+      reverseObject[robot[key]] = key;
+    }
+  }
+
+  return reverseObject;
 }
 
 module.exports = inverseRobot;
