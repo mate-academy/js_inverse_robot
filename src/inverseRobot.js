7e46998702dc47937c913ca4cb12fde4ad63f1e0
Eugene Chornyi <winheisbox@gmail.com>
2021-10-14 18:06:47 +0300

    HomeWork

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,6 +25,17 @@
  */
 function inverseRobot(robot) {
   // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    if (Object.keys(newRobot).includes(robot[key])) {
+      return null;
+    }
+
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
