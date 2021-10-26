078fc08719ad08ec126f5bb1b537f92acca6d531
Vasiliy <voronvasia@gmail.com>
2021-10-26 06:50:06 +0300

    inv keys and value

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,13 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
