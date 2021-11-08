02e9aa1c58ef9352ec5957ad90ef93a2d06d1731
Ihor Poliakov <junglqq@gmail.com>
2021-11-08 21:52:59 +0200

    add

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const fixedRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (fixedRobot.hasOwnProperty(value)) {
+      return null;
+    }
+
+    fixedRobot[value] = key;
+  }
+
+  return fixedRobot;
 }
 
 module.exports = inverseRobot;
