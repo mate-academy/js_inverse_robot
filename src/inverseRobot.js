4e9a5af70df83bcb0ed5d3e2177154f3dc90fd83
WorkAccCom <work.acc.com@gmail.com>
2021-11-10 12:49:50 +0200

    Second solution: multi-level loop deleted

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -28,17 +28,13 @@ function inverseRobot(robot) {
   const keys = Object.values(robot);
   const values = Object.keys(robot);
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = i + 1; j < keys.length; j++) {
-      if (keys[i] === keys[j]) {
-        return null;
-      }
-    }
-  }
-
   const inversedRobot = {};
 
   for (let i = 0; i <= keys.length; i++) {
+    if (inversedRobot[keys[i]]) {
+      return null;
+    }
+
     inversedRobot[keys[i]] = values[i];
   }
 
