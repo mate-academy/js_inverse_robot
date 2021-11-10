522c598504204c74c5e055d83bfd01383848757c
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-11-10 16:58:25 +0200

    fix

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,17 +27,16 @@
 function inverseRobot(robot) {
   const keys = Object.values(robot);
   const values = Object.keys(robot);
-  const result = {};
+  const inversedRobot = {};
 
   for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
+    if (inversedRobot.hasOwnProperty(keys[i])) {
+      return null;
+    }
+    inversedRobot[keys[i]] = values[i];
   }
 
-  if (Object.keys(result).length !== keys.length) {
-    return null;
-  }
-
-  return result;
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
