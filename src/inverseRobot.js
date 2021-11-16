f45cd28a26f72699e0d726be0300d717bd1c9515
Skidan Alexander(notebook) <xpromt@gmail.com>
2021-11-16 13:50:49 +0200

    result

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reverseRobot = {};
+
+  for (const key in robot) {
+    if (reverseRobot[robot[key]] !== undefined) {
+      return null;
+    }
+    reverseRobot[robot[key]] = key;
+  }
+
+  return reverseRobot;
 }
 
 module.exports = inverseRobot;
