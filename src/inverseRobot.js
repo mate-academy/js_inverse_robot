f0b94e278b609cb13bde6f9ba8c0441862569d44
Samson Sim <samson.sim@ukr.net>
2021-12-13 19:26:59 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reverseRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in reverseRobot) {
+      return null;
+    }
+    reverseRobot[robot[key]] = key;
+  }
+
+  return reverseRobot;
 }
 
 module.exports = inverseRobot;
