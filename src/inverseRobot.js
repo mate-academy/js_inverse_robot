a54711ec0e0029cbbcaa407caec44bf97a1ba1f1
Basil-Panasiuk <vasylpanasjuk2001@gmail.com>
2021-12-14 16:47:16 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const ob = {};
+
+  for (const key in robot) {
+    ob[robot[key]] = key;
+  }
+
+  if (Object.keys(ob).length !== Object.keys(robot).length) {
+    return null;
+  }
+
+  return ob;
 }
 
 module.exports = inverseRobot;
