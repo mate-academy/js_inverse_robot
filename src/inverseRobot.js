28fbe616882aa51303ec00f44da646bcb3d8ebbe
Valeria Valuyskaya <valeriavaluyskaya@gmail.com>
2021-12-14 17:25:02 +0200

    done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const robotInverse = {};
+
+  for (const key in robot) {
+    if (robot[key] in robotInverse) {
+      return null;
+    }
+
+    const inverseKey = robot[key];
+
+    robotInverse[inverseKey] = key;
+  }
+
+  return robotInverse;
 }
 
 module.exports = inverseRobot;
