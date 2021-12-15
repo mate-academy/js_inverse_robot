fd984e1aa259bed2c773cc642def028a6fafb567
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2021-12-15 15:26:38 +0200

    solution_v1.0

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+
+  for (const key in robot) {
+    if (invertedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+    invertedRobot[robot[key]] = key;
+  };
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
