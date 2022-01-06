2e0b6018d7e6d6cb2d2578e54cc25c99cce72806
Ladyha Roman <ladrom.ua@gmail.com>
2022-01-06 13:38:14 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const copyRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in copyRobot) {
+      return null;
+    }
+    copyRobot[robot[key]] = key;
+  }
+
+  return copyRobot;
 }
 
 module.exports = inverseRobot;
