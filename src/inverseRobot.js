b28e5ac48490119c815abf355ea5582e062a672e
Anton Hanetskyi <antonganetskiy@gmail.com>
2022-02-09 14:53:52 +0200

    comments was fixed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,15 +25,13 @@
  */
 function inverseRobot(robot) {
   const inversRobot = {};
-  const keys = Object.keys(robot);
-  const values = Object.values(robot);
 
-  for (let i = 0; i < keys.length; i++) {
-    if (inversRobot.hasOwnProperty(values[i])) {
+  for (const key in robot) {
+    if (inversRobot[robot[key]]) {
       return null;
-    } else {
-      inversRobot[values[i]] = keys[i];
     }
+
+    inversRobot[robot[key]] = key;
   }
 
   return inversRobot;
