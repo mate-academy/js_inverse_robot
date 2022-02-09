574d1eccb7472e4637c9e017a30c35a7570aa07b
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-09 14:25:19 +0200

    Inverse robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const newRob = {};
+
+  for (const key in robot) {
+    if (newRob.hasOwnProperty(robot[key])) {
+      return null;
+    }
+    newRob[robot[key]] = key;
+  }
+
+  return newRob;
 }
 
 module.exports = inverseRobot;
