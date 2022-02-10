7ec9833ba5a057b4f0cb509ba99bb3f85827f16b
Anna Serdechna <anserdechna@gmail.com>
2022-02-10 15:02:31 +0200

    done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,7 +27,10 @@
 function inverseRobot(robot) {
   const result = {};
 
- 
+  for (const key in robot) {
+    if (robot[key] in result) {
+      return null;
+    }
 
     result[robot[key]] = key;
   }
