bceee36c36512ab230ad175be3260aaff5aaaa30
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-02-17 08:54:18 +0200

    Solution for js_inverse_robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in invertedRobot) {
+      return null;
+    }
+
+    invertedRobot[robot[key]] = key;
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
