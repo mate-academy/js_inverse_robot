ce079c85b6957578b492059421330ecc6020acb9
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-31 13:38:33 +0300

    Finish the task - Inverse robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,23 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const arr = Object.entries(robot);
+  const duplicateValues = {};
+
+  arr.forEach(element => {
+    duplicateValues[element[1]] = (duplicateValues[element[1]] || 0) + 1;
+  });
+
+  const amountDuplicateValues = Object.values(duplicateValues);
+
+  if (amountDuplicateValues.some(el => el > 1)) {
+    return null;
+  }
+
+  const reverseKeyAndValue = arr
+    .map(element => [element[1], element[0]]).reverse();
+
+  return Object.fromEntries(reverseKeyAndValue);
 }
 
 module.exports = inverseRobot;
