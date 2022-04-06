b940d8ff2967d3bb0145cdc0d9bae783a4941d96
Misha Kravtsov <aniahucm@gmail.com>
2022-04-06 18:38:43 +0300

    simplify solve

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -26,14 +26,12 @@
 function inverseRobot(robot) {
   const outerObject = {};
 
-  const keys = Object.keys(robot);
-  const values = Object.values(robot);
-
-  for (let i = 0; i < keys.length; i++) {
-    if (values.indexOf(values[i], i + 1) !== -1) {
+  for (const key in robot) {
+    if (outerObject.hasOwnProperty(robot[key])) {
       return null;
     }
-    outerObject[robot[keys[i]]] = keys[i];
+
+    outerObject[robot[key]] = key;
   }
 
   return outerObject;
