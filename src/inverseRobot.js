e46b7670a2916246f1bc49064ea9d8e5a47eb84b
Hovera-Liubomyr <ljusuk@gmail.com>
2022-04-13 13:38:37 +0300

    inverse_robot_1

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,19 +24,13 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  const mas = Object.values(robot);
-
-  for (let i = 0; i < mas.length; i++) {
-    for (let j = 1; j < mas.length; j++) {
-      if (mas[i] === mas[i + j]) {
-        return null;
-      }
-    }
-  }
-
   const result = {};
 
   for (const key in robot) {
+    if (robot[key] in result) {
+      return null;
+    }
+
     result[robot[key]] = key;
   }
 
