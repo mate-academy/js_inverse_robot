565e14c4a831f4c9fa317baaf8a464d02d92a2c4
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-04-27 13:40:48 +0200

    invers robot done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const inverse = {};
+
+  for (const [key, value] of Object.entries(robot)) {
+    if (inverse[value]) {
+      return null;
+    }
+    inverse[value] = key;
+  }
+
+  return inverse;
 }
 
 module.exports = inverseRobot;
