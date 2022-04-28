6224f3ca6f0d3f888a4f00b5b3c754d72b57b7f7
Server <isr.lviv2021@gmail.com>
2022-04-28 16:53:52 +0300

    inverse Robot done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,18 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const res = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (res.hasOwnProperty(value)) {
+      return null;
+    }
+    res[value] = key;
+  }
+
+  return res;
 }
 
 module.exports = inverseRobot;
