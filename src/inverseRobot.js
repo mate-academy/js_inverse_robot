fcb1390d7c006a47dcb24355a01387ef00a10fc1
olha slipushchenko <olhaslipushchenko@Air-olha.homenet.telecomitalia.it>
2022-05-06 11:05:25 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const repaireRobot = {};
+
+  for (const detail in robot) {
+    if (repaireRobot.hasOwnProperty(robot[detail])) {
+      return null;
+    }
+    repaireRobot[robot[detail]] = detail;
+  }
+
+  return repaireRobot;
 }
 
 module.exports = inverseRobot;
