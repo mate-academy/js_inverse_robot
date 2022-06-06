1bea9c4a26fdb3ff9b923f54ffdc6dbd06724296
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-06 18:00:24 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -20,11 +20,24 @@
  *
  *
  * @param {object} robot
+ * @param {object} invertedRobot
  *
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (invertedRobot.hasOwnProperty(value)) {
+      return null;
+    }
+
+    invertedRobot[value] = key;
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
