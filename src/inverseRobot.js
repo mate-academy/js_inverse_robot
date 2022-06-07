3360125c9c8dd34fd1d9303476722bb81cd02498
Lesya Savka <lesyasimple@gmail.com>
2022-06-07 18:42:22 +0300

    some changes

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,7 +27,7 @@ function inverseRobot(robot) {
   const invertedRobot = {};
 
   for (const key in robot) {
-    if (robot[key] in invertedRobot) {
+    if (invertedRobot.hasOwnProperty(robot[key])) {
       return null;
     }
 
