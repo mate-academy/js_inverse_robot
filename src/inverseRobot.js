5ccf8f07c0f5e9baf778b806ca9dd7104a39077b
Yaroslav Baiduk <byaa99@gmail.com>
2022-06-07 15:41:04 +0300

    Compleated

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,20 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const newObj = {};
+  const arrayCheck = [];
+
+  for (const prop in robot) {
+    if (!arrayCheck.includes(robot[prop])) {
+      arrayCheck.push(robot[prop]);
+    } else {
+      return null;
+    }
+
+    newObj[robot[prop]] = prop;
+  }
+
+  return newObj;
 }
 
 module.exports = inverseRobot;
