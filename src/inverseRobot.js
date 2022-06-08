31085a75cb348272d2a9fdf7d35b4f870963e3d1
DanaKhromenko <khromenkodana@gmail.com>
2022-06-08 19:20:27 +0300

    Implemented js_inverse_robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,24 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  if (!robot || robot === {}) {
+    return robot;
+  }
+
+  const reversedRobot = {};
+
+  for (const key in robot) {
+    const actualKey = robot[key];
+    const actualValue = key;
+
+    if (reversedRobot[actualKey]) {
+      return null;
+    }
+
+    reversedRobot[actualKey] = actualValue;
+  }
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
