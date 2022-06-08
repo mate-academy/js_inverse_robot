793de7bde673be17706e046a94cc8f7c664d5e6e
viktorlyt <viktorlyt@gmail.com>
2022-06-08 17:27:33 +0300

    after mentor review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,11 +27,13 @@ function inverseRobot(robot) {
   const newRobot = {};
 
   for (const key in robot) {
-    if (newRobot.hasOwnProperty(robot[key])) {
+    const newProp = robot[key];
+
+    if (newRobot.hasOwnProperty(newProp)) {
       return null;
     }
 
-    newRobot[robot[key]] = key;
+    newRobot[newProp] = key;
   }
 
   return newRobot;
