152eca3e62017062024fa2690fe8ccc2126ca23a
Taras Kozii <taras_kozii@ukr.net>
2022-06-09 13:54:02 +0300

    solution v01

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,29 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const valuesRobot = Object.values(robot);
+  let inverse = {};
+  let sum = 0;
+
+  for (const key in robot) {
+    inverse[robot[key]] = key;
+  }
+
+  for (const key in inverse) {
+    valuesRobot.forEach(function(elem) {
+      if (elem === key) {
+        sum++;
+      }
+    });
+
+    if (sum > 1) {
+      inverse = null;
+    } else {
+      sum = 0;
+    }
+  }
+
+  return inverse;
 }
 
 module.exports = inverseRobot;
