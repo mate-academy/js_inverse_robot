3b085c85bc1b5019f32c40d7901e14dd59b97a8b
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-07-05 08:29:13 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,6 +25,17 @@
  */
 function inverseRobot(robot) {
   // write code here
+  const reverseRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in reverseRobot) {
+      return null;
+    }
+
+    reverseRobot[robot[key]] = key;
+  }
+
+  return reverseRobot;
 }
 
 module.exports = inverseRobot;
