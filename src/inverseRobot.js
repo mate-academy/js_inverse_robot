b73e91475345d35914f66131f57b4606e3d9e1f5
fenderukr <priststoreua@gmail.com>
2022-07-06 16:18:58 +0300

    inverseRobot0.2

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,12 +27,19 @@
 function inverseRobot(robot) {
   const newObj = {};
 
+  // for (const key in robot) {
+  //   if (!newObj[robot[key]]) {
+  //     newObj[robot[key]] = key;
+  //   } else {
+  //     return null;
+  //   }
+  // }
   for (const key in robot) {
-    if (!newObj[robot[key]]) {
-      newObj[robot[key]] = key;
-    } else {
+    if (newObj[robot[key]]) {
       return null;
     }
+
+    newObj[robot[key]] = key;
   }
 
   return newObj;
