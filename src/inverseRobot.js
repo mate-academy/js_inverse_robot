b200e6bbc17198495c964a898e48410dafa300cb
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-08-03 18:54:43 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,8 +24,9 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  const obj = Object.entries(robot).map(([key, value]) => [value, key]);
-  const result = Object.fromEntries(obj);
+  const robotEntries = Object.entries(robot).map(([key, value]) =>
+    [value, key]);
+  const result = Object.fromEntries(robotEntries);
 
   if (Object.keys(result).length < Object.values(robot).length) {
     return null;
