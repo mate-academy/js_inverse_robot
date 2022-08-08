ed3b2f3e9039fdd72a2ad6dea7f58031b0e7fdcb
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-08 16:01:09 +0300

    draft

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -23,8 +23,67 @@
  *
  * @return {object}
  */
+
+// const kolli = {
+//   Kolli: 'name',
+//   123: 'chipVer',
+//   3: 'wheels',
+// };
+
+// const robert = {
+//   Robert: 'name',
+//   123: 'chipVer',
+//   113: 'chipVer',
+// };
+
+// const t = {
+//   name: 'Bob',
+//   fullName: 'Bob Acad',
+//   age: 32,
+//   gender: 'male',
+//   interests: 'music',
+//   nickname: 'Bob',
+// };
+
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  // проблема в том что ключи могут быть в разных местах а не подряд
+  // вот val[i] === val[i + 1]
+  // не срабатывает
+  // const val = Object.values(robot);
+  // console.log(val);
+  // const keys = Object.keys(robot);
+  // console.log(keys);
+
+  // for (let i = 0; i < val.length; i++) {
+  //   if (val[i] === val[i + 1]) {
+  //     return null;
+  //   }
+  // }
+  // for (const key in robot) {
+  //   console.log(robot[key]);
+  // }
+
+  for (const key in robot) {
+    // if (condition) {
+    //   // console.log(robot[key] );
+    // }
+
+    newRobot[robot[key]] = key;
+    // console.log(robot[key] === newRobot[key]);
+    // console.log(newRobot[key]);
+
+    // for (let i = 0; i < newRobot.length; i++) {
+    //     console.log(i);
+    // }
+  }
+
+  return newRobot;
 }
+
+// console.log(inverseRobot(t));
+// console.log(inverseRobot(kolli));
+// console.log(inverseRobot(robert));
 
 module.exports = inverseRobot;
