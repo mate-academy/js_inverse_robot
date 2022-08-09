f2431c7b8a429dbe1d3852ae7e705a0072f9f479
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-09 16:36:57 +0300

    rename variables

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,19 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const inverse = {};
+  const inverseKeys = Object.values(robot);
+  const inverseValues = Object.keys(robot);
+
+  for (let i = 0; i < inverseKeys.length; i++) {
+    if (inverse[inverseKeys[i]]) {
+      return null;
+    }
+
+    inverse[inverseKeys[i]] = inverseValues[i];
+  }
+
+  return inverse;
 }
 
 module.exports = inverseRobot;
