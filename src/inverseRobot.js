fadbd9d5bdb78a93d3fcd201ccb39ea4e30bd0d4
Dasha Protsko <protscodasha@icloud.com>
2022-08-15 20:15:47 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,30 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const newObj = checkRepeat(Object.values(robot));
+
+  if (newObj === null) {
+    return null;
+  }
+
+  for (const key in robot) {
+    newObj[robot[key]] = key;
+  }
+
+  return newObj;
 }
 
+function checkRepeat(values) {
+  for (let i = 0; i < values.length; i++) {
+    const currentValue = values[i];
+
+    for (let j = i + 1; j < values.length; j++) {
+      if (currentValue === values[j]) {
+        return null;
+      }
+    }
+  }
+
+  return {};
+};
 module.exports = inverseRobot;
