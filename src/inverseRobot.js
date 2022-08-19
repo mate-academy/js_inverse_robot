2a74bb84a7b972ef0140f5ea941af1842fc59962
EhorSaprykin <astralik1993@gmail.com>
2022-08-19 13:21:25 +0300

    Reverse done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,22 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reverseRobot = {};
+
+  for (const key in robot) {
+    const names = robot[key];
+    const values = key;
+
+    if (reverseRobot[names]) {
+      return null;
+    }
+
+    reverseRobot[names] = values;
+  }
+
+  return reverseRobot;
 }
+
+inverseRobot();
 
 module.exports = inverseRobot;
