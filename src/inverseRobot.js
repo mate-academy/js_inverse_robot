542527102559f7009f426461a3a3c1602894d0a1
Kolya Movchan <kmovchan261@gmail.com>
2022-08-31 21:07:10 +0300

    inverseRobot.js done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,21 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const robotSwitched = {};
+  const values = Object.values(robot);
+  const keys = Object.keys(robot);
+
+  const unique = Array.from(new Set(values));
+
+  if (values.length > unique.length) {
+    return null;
+  }
+
+  values.forEach((value, key) => {
+    robotSwitched[value] = keys[key];
+  });
+
+  return robotSwitched;
 }
 
 module.exports = inverseRobot;
