7a10d09760809efdc881fd4850432232c4d8472f
Volodymyr Klymkiv <volodymyr.klymkiv@gmail.com>
2022-09-12 15:11:36 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -25,6 +25,19 @@
  */
 function inverseRobot(robot) {
   // write code here
+  const obj = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (obj.hasOwnProperty(value)) {
+      return null;
+    }
+
+    obj[value] = key;
+  }
+
+  return obj;
 }
 
 module.exports = inverseRobot;
