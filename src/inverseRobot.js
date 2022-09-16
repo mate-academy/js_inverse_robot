4df710333e50dc276e2c88a5568d9ebc07a52c52
Maksym845 <mzuravel845@gmail.com>
2022-09-16 10:04:26 +0300

    fix was added

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,18 +24,12 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  const keys = Object.values(robot);
   const res = {};
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = i + 1; j < keys.length; j++) {
-      if (keys[i] === keys[j]) {
-        return null;
-      }
-    }
-  }
-
   for (const key in robot) {
+    if (res.hasOwnProperty(robot[key])) {
+      return null;
+    }
     res[robot[key]] = key;
   }
 
