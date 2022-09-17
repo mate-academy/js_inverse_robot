6c441d58f7b89044dc577b8dc9af4018e368ed8e
Kalmykov Mikhail <kalmykov.mikhail2@gmail.com>
2022-09-17 17:00:09 +0300

    task was solved

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const reversRobot = {};
+
+  for (const key in robot) {
+    if (reversRobot[robot[key]]) {
+      return null;
+    }
+
+    reversRobot[robot[key]] = key;
+  }
+
+  return reversRobot;
 }
 
 module.exports = inverseRobot;
