318dd69d99a890de3e69b24e241414981a351348
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-09-19 16:15:54 +0300

    Implement inverseRobot function

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,24 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  if (hasRepeatedValues(robot)) {
+    return null;
+  }
+
+  const inversed = {};
+
+  for (const key in robot) {
+    inversed[robot[key]] = key;
+  }
+
+  return inversed;
+}
+
+function hasRepeatedValues(obj) {
+  const values = Object.values(obj);
+  const uniqueValues = new Set(values);
+
+  return !(values.length === uniqueValues.size);
 }
 
 module.exports = inverseRobot;
