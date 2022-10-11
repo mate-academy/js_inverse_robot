4871b2fdeca6c2e4054c54f8bd0f175eca547eaf
Maxim Stashuk <maxim.stashuk@gmail.com>
2022-10-11 13:43:17 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,24 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const spysok = [];
+  const arr = [];
+  const result = {};
+
+  for (const element in robot) {
+    spysok.push(robot[element]);
+    result[robot[element]] = element;
+  }
+
+  for (const str of spysok) {
+    if (!arr.includes(str)) {
+      arr.push(str);
+    } else {
+      return null;
+    }
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
