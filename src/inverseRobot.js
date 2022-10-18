f59e9b0d48688dcc6d1c53860558d9f13e96a232
Volodymyr <vovannty@ukr.net>
2022-10-18 12:16:07 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,16 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const active = {};
+
+  for (const [key, value] of Object.entries(robot)) {
+    if (active.hasOwnProperty(robot[key])) {
+      return null;
+    }
+    active[value] = key;
+  }
+
+  return active;
 }
 
 module.exports = inverseRobot;
