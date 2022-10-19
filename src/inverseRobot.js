225760ae0c7d58be9f2bcf7ee6a75128f424aeb8
Alex Tsimba <alexander.tsimba@gmail.com>
2022-10-19 16:08:38 +0300

    inverseRobot task is done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
-}
+  const arr = Object.entries(robot);
+  const result = {};
 
+  for (let i = 0; i < arr.length; i++) {
+    result[arr[i][1]] = arr[i][0];
+  }
+
+  if (Object.keys(robot).length !== Object.keys(result).length) {
+    return null;
+  }
+
+  return result;
+}
 module.exports = inverseRobot;
