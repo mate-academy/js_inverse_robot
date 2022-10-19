84e7507cde0fe8a04c99c7593180bf6a1b971762
AndreyChernysh <achernysh744@gmail.com>
2022-10-19 20:07:07 +0300

    commit solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,7 +24,17 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  // write code here
+  const copy = {};
+
+  for (const key in robot) {
+    if (copy[robot[key]]) {
+      return null;
+    }
+
+    copy[robot[key]] = key;
+  }
+
+  return copy;
 }
 
 module.exports = inverseRobot;
