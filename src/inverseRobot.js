f218a908c03706ddad05ad344a8aeb2feafcb196
Vlad Didkovskiy <vladgreen.vg@gmail.com>
2022-10-20 14:03:46 +0300

    hasOwnProperty

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -27,7 +27,7 @@ function inverseRobot(robot) {
   const fixedRobot = {};
 
   for (const key in robot) {
-    if (fixedRobot[robot[key]]) {
+    if (fixedRobot.hasOwnProperty(robot[key])) {
       return null;
     }
 
