19a80c1bc27a7f3cd9fd70e078c5ee33acc3e909
Danyla Krygin <underworldsan@gmail.com>
2022-10-20 12:12:19 +0300

    Made correction

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -24,23 +24,14 @@
  * @return {object}
  */
 function inverseRobot(robot) {
-  const properties = Object.values(robot);
-  const values = Object.keys(robot);
-
-  for (let i = 0; i < properties.length; i++) {
-    for (let j = 0; j < values.length; j++) {
-      if (i !== j) {
-        if (properties[i] === properties[j] || values[i] === values[j]) {
-          return null;
-        }
-      }
-    }
-  }
-
   const newRobot = {};
 
-  for (let i = 0; i < properties.length; i++) {
-    newRobot[properties[i]] = values[i];
+  for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    newRobot[robot[key]] = key;
   }
 
   return newRobot;
