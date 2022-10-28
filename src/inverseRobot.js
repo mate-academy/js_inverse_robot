c5b11edf1b1277b4038f91cdd6991673d4863424
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-10-28 14:04:24 +0300

    initial

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,19 @@
 
 function inverseRobot(robot) {
   // write code here
+  const lengthOriginal = Array.from(new Set(Object.values(robot))).length;
+
+  if (lengthOriginal === Object.values(robot).length) {
+    const inversed = {};
+
+    Object.keys(robot).forEach(key => {
+      inversed[robot[key]] = key;
+    });
+
+    return inversed;
+  }
+
+  return null;
 }
 
 module.exports = inverseRobot;
