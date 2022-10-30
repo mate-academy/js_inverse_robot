4f171bb0e1aa47fcdbbd0c0ac3050af788a287d8
Dmitriy-Kovalenko <dmitrijkovalenko@gmail.com>
2022-10-30 21:02:31 +0200

    test Ok

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const fixRobot = {};
+
+  const arrValues = Object.values(robot);
+  const arrKeys = Object.keys(robot);
+
+  for (const key in robot) {
+    if (arrValues.filter(n => n === robot[key]).length > 1
+    || arrKeys.filter(n => n === key).length > 1) {
+      return null;
+    }
+    fixRobot[robot[key]] = key;
+  }
+
+  return fixRobot;
 }
 
 module.exports = inverseRobot;
