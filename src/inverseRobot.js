d0ba40cddbf7fa172274954715d4d3e535545fa5
zbyn3k <coding707@mailbox.org>
2022-11-05 12:12:06 +0100

    Added the solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newObject = {};
+
+  for (const key in robot) {
+    const newKey = robot[key];
+
+    newObject[newKey] = key;
+  }
+
+  // duplicate values in robot can only ever create one unique key in newObject
+  if (Object.values(newObject).length !== Object.values(robot).length) {
+    return null;
+  }
+
+  return newObject;
 }
 
 module.exports = inverseRobot;
