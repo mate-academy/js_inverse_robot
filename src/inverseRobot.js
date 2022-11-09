84fc26c1f88472974a4b8c2cc564767289afc425
Maksym Kyivskyi <weikmaks@gmail.com>
2022-11-09 13:27:10 +0200

    inverseRobot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const arr = Object.entries(robot);
+
+  const revers = arr.map(function(i) {
+    return i.reverse();
+  });
+
+  const objRevers = Object.fromEntries(revers);
+
+  if (Object.keys(objRevers).length !== Object.keys(robot).length) {
+    return null;
+  }
+
+  return objRevers;
 }
 
 module.exports = inverseRobot;
