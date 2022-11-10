f00602fc4234d4d03f9f5fdab4d1f13a72d23eb3
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2022-11-10 12:53:35 +0200

    fSolution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  let newRobot = {};
+
+  const values = Object.values(robot);
+
+  for (let index = 0; index < values.length; index++) {
+    if (values.includes(values[index], index + 1)) {
+      newRobot = null;
+
+      return newRobot;
+    }
+  }
+
+  for (const key in robot) {
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
