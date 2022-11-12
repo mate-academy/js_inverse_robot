94a70b2de71e5bc03e3aa0f0275cde7cdcda7a2d
Sergii Mishchenko <serge.mishchenko@gmail.com>
2022-11-12 23:54:55 +0300

    New Robot created

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const values = (Object.values(robot));
+
+  for (let i = 0; i < values.length; i++) {
+    for (let y = i + 1; y < values.length; y++) {
+      if (values[i] === values[y]) {
+        return null;
+      }
+    }
+  }
+
+  const newRobot = {};
+
+  for (const key in robot) {
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
