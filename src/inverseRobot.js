54d64520c6d50af57ca29c1a7cad6d50dac09a1b
mykolachupyak <chupyak@gmail.com>
2022-11-14 16:17:26 +0200

    function inverseRobot was implemented

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.keys(robot);
+  const values = Object.values(robot);
+  const newRobot = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    newRobot[values[i]] = keys[i];
+  }
+
+  if (Object.keys(newRobot).length !== keys.length) {
+    return null;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
