d2c352a3e8eb74d4c3dcae484ca6df34aad80088
OksanaBaloh <obaloh1108@gmail.com>
2022-11-15 15:02:06 +0200

    Solution was corrected

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,19 +7,14 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
-  const keys = Object.values(robot);
-  const uniqueKeys = new Set(keys);
-
-  if (keys.length !== uniqueKeys.size) {
-    return null;
-  }
-
-  const values = Object.keys(robot);
   const invertedRobot = {};
 
-  for (let i = 0; i < keys.length; i++) {
-    invertedRobot[keys[i]] = values[i];
+  for (const key in robot) {
+    if (invertedRobot[robot[key]]) {
+      return null;
+    }
+
+    invertedRobot[robot[key]] = key;
   }
 
   return invertedRobot;
