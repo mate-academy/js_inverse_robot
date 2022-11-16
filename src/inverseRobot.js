a8f8f1ae344ed566d02d81b82be0daa7ff5afe60
mykolachupyak <chupyak@gmail.com>
2022-11-16 13:55:54 +0200

    added condition in the loop

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,11 +10,10 @@ function inverseRobot(robot) {
   const newRobot = {};
 
   for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
     newRobot[robot[key]] = key;
-  }
-
-  if (Object.keys(newRobot).length !== Object.keys(robot).length) {
-    return null;
   }
 
   return newRobot;
