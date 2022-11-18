106d89da140d93a80f752a73d346419fd01bc502
Aleksey-Aleksey <l0734062646@gmail.com>
2022-11-18 18:53:38 +0200

    done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,16 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inverse = {};
+
+  for (const key in robot) {
+    if (inverse[robot[key]]) {
+      return null;
+    }
+    inverse[robot[key]] = key;
+  }
+
+  return inverse;
 }
 
 module.exports = inverseRobot;
