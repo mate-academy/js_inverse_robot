d41331039c064ad9451dbd35212677b7b9bb88b2
mr-valentos <nerpavlad@gmail.com>
2022-11-19 16:38:52 +0200

    inverse robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  let newRobot = {};
+
+  for (const key in robot) {
+    if (newRobot[robot[key]]) {
+      newRobot = null;
+      break;
+    }
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
