f497de7e4579c70a8694dff6d62d08a725e899e0
Roman Korchovskiy <korchovskiy@MacBook-Pro-Roman.local>
2022-11-20 16:22:24 +0200

    script created

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /*
@@ -8,6 +9,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inverstedRobot = {};
+
+  for (const key in robot) {
+    if (inverstedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    inverseRobot[robot[key]] = key;
+  }
+
+  return inverseRobot;
 }
 
 module.exports = inverseRobot;
