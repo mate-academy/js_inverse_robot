ef3e2debc41b8311a347deb02f56941b77b36254
Nadiia Kotliar <eknakaron@gmail.com>
2022-11-21 17:31:09 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,16 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  for (const kay in robot) {
+    if (robot.hasOwnProperty(robot[kay])) {
+      return null;
+    }
+
+    robot[robot[kay]] = kay;
+    delete robot[kay];
+  }
+
+  return robot;
 }
 
 module.exports = inverseRobot;
