8012e2f7d71d49e8b5148d01543f5a9057b2f4d6
JzSol <111354281+JzSol@users.noreply.github.com>
2022-11-23 18:56:44 +0200

    Finished task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
   // write code here
+  const revese = {};
+
+  for (const keys in robot) {
+    if (revese.hasOwnProperty(robot[keys])) {
+      return null;
+    }
+    revese[robot[keys]] = keys;
+  }
+
+  return revese;
 }
 
 module.exports = inverseRobot;
