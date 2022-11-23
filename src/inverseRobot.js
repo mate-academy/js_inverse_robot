fa5e16b6adafef3d627bf2e6cbb64fce4f52a64a
Romeo Kiss <kiss.romeo.richard@gmail.com>
2022-11-23 16:34:33 +0200

    Solved according to review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,31 +9,16 @@
 
 function inverseRobot(robot) {
   // write code here
-  let objects = JSON.stringify(robot);
+  const newRobot = {};
 
-  objects = Object.entries(robot);
-
-  const objValues = Object.values(robot);
-
-  function hasDuplicates(arr) {
-    return arr.some(x => arr.indexOf(x) !== arr.lastIndexOf(x));
-  }
-
-  for (let i = 0; i < objValues.length - 1; i++) {
-    for (let j = 0; j < objects.length - 1; j++) {
-      if (objValues[i] === objects[j] || objects[i][1] === objects[i + 1][1]
-        || objValues[i] === objValues[i + 1] || hasDuplicates(objValues)) {
-        return null;
-      } else {
-        objects = Object.fromEntries(Object.entries(robot)
-          .map(([key, val]) => [val, key]));
-
-        return objects;
-      }
+  for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
     }
+    newRobot[robot[key]] = key;
   }
 
-  return robot;
+  return newRobot;
 };
 
 module.exports = inverseRobot;
