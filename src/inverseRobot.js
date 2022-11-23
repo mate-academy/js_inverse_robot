75a09beb06de2c846a5b21372b94c23c61a0a024
Anton Martynes <martynes1997@gmail.com>
2022-11-23 23:59:20 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,16 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const rightVersionOfRobot = {};
+
+  for (const key in robot) {
+    if (rightVersionOfRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+    rightVersionOfRobot[robot[key]] = key;
+  }
+
+  return rightVersionOfRobot;
 }
 
 module.exports = inverseRobot;
