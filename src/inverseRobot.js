dd3012918e3e971100d3842666c3f83074cbb496
JRijo <99342891+John-Stiff@users.noreply.github.com>
2022-11-24 01:29:09 +0000

    created function inverseRobots()

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,19 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const reversedRobot = {};
+
+  for (const key in robot) {
+    if (reversedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    };
+    reversedRobot[robot[key]] = key;
+  };
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
