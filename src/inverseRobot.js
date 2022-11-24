6531e7be3894a156896f86d69cdded35151a46de
Daniil Yakovlev <yakovlev.dan996@gmail.com>
2022-11-25 01:20:24 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,31 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inverseCopy = {};
+
+  const length = Object.keys(robot).length;
+
+  for (let i = 0; i < length; i++) {
+    let dupliCount = 0;
+
+    for (let j = 0; j < length; j++) {
+      if (Object.values(robot)[i] === Object.values(robot)[j]) {
+        dupliCount++;
+      }
+    }
+
+    if (dupliCount > 1) {
+      return null;
+    }
+  }
+
+  for (let i = 0; i < length; i++) {
+    const key = Object.keys(robot)[i];
+
+    inverseCopy[robot[key]] = key;
+  }
+
+  return inverseCopy;
 }
 
 module.exports = inverseRobot;
