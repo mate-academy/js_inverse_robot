662268fe04acb2ee00d4ff98533a1d9300767545
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2022-11-25 17:28:46 +0200

    Added inverseRobot function

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,28 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const givenValues = Object.values(robot);
+
+  for (let i = 0; i < givenValues.length; i++) {
+    const currentValue = givenValues[i];
+
+    for (let j = i + 1; j < givenValues.length; j++) {
+      if (givenValues[j] === currentValue) {
+        return null;
+      }
+    }
+  }
+
+  const givenKeys = Object.keys(robot);
+  const inversedProperties = [];
+
+  for (let x = 0; x < givenValues.length; x++) {
+    inversedProperties.push([givenValues[x], givenKeys[x]]);
+  }
+
+  const fixedRobot = Object.fromEntries(inversedProperties);
+
+  return fixedRobot;
 }
 
 module.exports = inverseRobot;
