005a2895d2968ec9f7343c859a2cc63a922ea094
Oleh Yablunovskyi <lifserv@gmail.com>
2022-11-25 18:37:47 +0200

    Requested changes were implemented

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,17 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  const newRobot = {};
+  const inversedRobot = {};
 
   for (const key in robot) {
-    if (newRobot.hasOwnProperty([robot[key]])) {
+    if (inversedRobot.hasOwnProperty([robot[key]])) {
       return null;
     }
 
-    newRobot[robot[key]] = key;
+    inversedRobot[robot[key]] = key;
   }
 
-  return newRobot;
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
