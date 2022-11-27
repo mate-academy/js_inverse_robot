fac934f461803788caacbb7539a59d156e35996f
Asus <kirilllubenchenko670@gmail.com>
2022-11-28 00:47:21 +0400

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,18 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newObj = {};
+  const mass = [];
+
+  for (const part in robot) {
+    if (mass.includes(robot[part])) {
+      return null;
+    }
+    newObj[robot[part]] = part;
+    mass.push(robot[part]);
+  }
+
+  return newObj;
 }
 
 module.exports = inverseRobot;
