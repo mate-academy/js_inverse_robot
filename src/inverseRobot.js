cbcef38aab9a01edcd5e725966a5d8dc107422cb
Vladyslav Krainyk <vladkraynyk2701@gmail.com>
2022-11-28 19:21:16 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const transpositionRobot = {};
+  const prop = Object.keys(robot).concat(Object.values(robot));
+
+  for (let i = 0; i < prop.length; i++) {
+    for (let j = i + 1; j < prop.length; j++) {
+      if (prop[i] === prop[j]) {
+        return null;
+      }
+    }
+  }
+
+  for (const key in robot) {
+    transpositionRobot[robot[key]] = key;
+  }
+
+  return transpositionRobot;
 }
 
 module.exports = inverseRobot;
