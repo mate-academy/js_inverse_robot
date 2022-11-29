89c46e0381d53d841c2cf1d7954894da1f072956
Vadym Prydatok <vadymprydatok@gmail.com>
2022-11-29 12:27:18 +0200

    robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const Obj = {};
+
+  for (const key in robot) {
+    if (Obj[robot[key]] === undefined) {
+      Obj[robot[key]] = key;
+    } else {
+      return null;
+    }
+  }
+
+  return Obj;
 }
 
 module.exports = inverseRobot;
