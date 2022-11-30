d8f1ae0b9403cf1c2e424b3f8a8315b604616a8a
Mykhailo Orlov <mihalikmihalik85@gmail.com>
2022-11-30 16:46:41 +0200

    correct mentor

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,20 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  const array = Object.values(robot);
+  const object = {};
 
-  for (let i = 0; i < array.length - 1; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] === array[j]) {
-        return null;
-      }
+  for (const key in robot) {
+    if (object[robot[key]]) {
+      return null;
     }
+
+    object[robot[key]] = key;
   }
 
-  const result = Object.fromEntries(Object.entries(robot)
-    .map(([key, value]) => [value, key]));
-
-  return result;
+  return object;
 }
 
 module.exports = inverseRobot;
