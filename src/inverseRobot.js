b18c472fa745e0304b8d777226e50fd176d66921
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2022-12-01 20:08:25 +0200

    I have done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const copy = { ...robot };
+
+  for (const key of Object.keys(copy)) {
+    if (copy.hasOwnProperty(copy[key])) {
+      return null;
+    }
+    copy[copy[key]] = key;
+    delete copy[key];
+  }
+
+  return copy;
 }
 
 module.exports = inverseRobot;
