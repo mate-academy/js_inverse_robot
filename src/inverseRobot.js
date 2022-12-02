50f2e98bb7dbfa945cc07b3281eba6ad3c595f01
Kateryna Stemblivska <ekaterina_09@ukr.net>
2022-12-02 16:35:03 +0200

    The second solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,17 +8,13 @@
 
 function inverseRobot(robot) {
   const repairRobot = {};
-  const keys = Object.keys(robot);
-  const values = Object.values(robot);
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = i + 1; j < keys.length; j++) {
-      if (values[i] === values[j]) {
-        return null;
-      }
+  for (const key in robot) {
+    if (robot[key] in repairRobot) {
+      return null;
     }
 
-    repairRobot[values[i]] = keys[i];
+    repairRobot[robot[key]] = key;
   }
 
   return repairRobot;
