e7c2e8a7d240af6f319c7eb83e91faa0d68043b6
Anna Akhalmosulishvili <akhala33@gmail.com>
2022-12-07 11:46:26 +0200

    inverseRobot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,24 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const objArr = Object.entries(robot);
+  const reverseArr = [];
+  const values = Object.values(robot);
+  const isDuplicate = values.some((item, index) =>
+    index !== values.indexOf(item)
+  );
+
+  if (!isDuplicate) {
+    for (let i = 0; i < objArr.length; i++) {
+      const reverse = objArr[i].reverse();
+
+      reverseArr.push(reverse);
+    }
+
+    return Object.fromEntries(reverseArr);
+  } else {
+    return null;
+  }
 }
 
 module.exports = inverseRobot;
