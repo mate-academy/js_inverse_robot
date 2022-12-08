248811d9c42ad1acd1b7721438b17547087ed64f
Sviatoslav Koziatynskyi <koziatynskyi2013@gmail.com>
2022-12-08 14:07:34 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,26 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const getRobot = {};
+  const arrRobot = Object.values(robot);
+
+  for (let i = 0; i < arrRobot.length; i++) {
+    if (arrRobot[i] === arrRobot[i + 1] || arrRobot[i + 5]) {
+      return null;
+    }
+  }
+
+  for (const i in robot) {
+    const value = i;
+    const key = robot[i];
+
+    getRobot[key] = value;
+  };
+
+  return getRobot;
 }
 
+// inverseRobot({
+//   Kolli: 'name', 123: 'chipVer', 3: 'wheels',
+// });
 module.exports = inverseRobot;
