72c3feff568d162c3c143cdff75c61d7467ba866
Roma Tsvindin <tsvindin1995@gmail.com>
2022-12-10 22:12:02 +0200

    Task was done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,16 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotValues = Object.values(robot);
+  const robotEnries = Object.entries(robot);
+  const unique = Array.from(new Set(robotValues));
+  const reverseEnries = robotEnries.map(a => a.reverse());
+
+  if (unique.length < robotValues.length) {
+    return null;
+  } else {
+    return (Object.fromEntries(reverseEnries.reverse()));
+  }
 }
 
 module.exports = inverseRobot;
