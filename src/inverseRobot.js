184a7921eb7dfbf5815df230652e395ac91ef212
Karadzha Mykola <nicholaskaradzha@gmail.com>
2022-12-16 12:23:12 +0000

    task completed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+  const keys = Object.keys(robot);
+  const values = Object.values(robot);
+
+  for (let i = keys.length - 1; i >= 0; i--) {
+    if (newRobot.hasOwnProperty(values[i])) {
+      return null;
+    }
+    newRobot[values[i]] = keys[i];
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
