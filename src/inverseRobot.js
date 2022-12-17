faf63f1d3c819b7eea9484fd480a6aed867c9349
yana-kotsulym <yanak7170@gmail.com>
2022-12-17 11:38:58 +0100

    Fixes after code review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,19 +9,15 @@
 function inverseRobot(robot) {
   const robotValues = Object.values(robot);
   const robotKeys = Object.keys(robot);
-  let newRobot = {};
+  const newRobot = {};
 
   for (let i = 0; i < robotValues.length; i++) {
     const type = robotValues[i];
 
-    newRobot[type] = robotKeys[i];
-  }
-
-  for (let i = 0; i < robotValues.length; i++) {
-    for (let n = i + 1; n < robotValues.length; n++) {
-      if (robotValues[i] === robotValues[n]) {
-        newRobot = null;
-      }
+    if ([type] in newRobot) {
+      return null;
+    } else {
+      newRobot[type] = robotKeys[i];
     }
   }
 
