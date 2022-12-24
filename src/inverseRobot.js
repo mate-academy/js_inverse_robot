0ae7c34c7a76a48f84e9153d4a7991c36db2b151
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2022-12-24 20:55:56 +0200

    add solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const values = Object.values(robot);
+  const keys = Object.keys(robot);
+
+  for (const elem of values) {
+    if (values.indexOf(elem) !== values.lastIndexOf(elem)) {
+      return null;
+    }
+  }
+
+  const resultObj = {};
+
+  for (let i = 0; i < values.length; i++) {
+    resultObj[values[i]] = keys[i];
+  }
+
+  return resultObj;
 }
 
 module.exports = inverseRobot;
