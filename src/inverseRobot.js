6e8913ac8a676a9e16b71f282487e52ecf029215
gretoq <dovhun3d@gmail.com>
2022-12-24 23:23:56 +0200

    Keys and values are replaced inside valid objects

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const normRobot = {}; // Declaring new object
+  const value = []; // Assign an array which will save all future keys
+
+  // Checking each element of object, if we already have
+  // the key with that name we brake our function and
+  // will return 'null'
+  // Else we change places with values
+  for (const key in robot) {
+    if (value.includes(robot[key])) {
+      return null;
+    } else {
+      value.push(robot[key]);
+    }
+
+    normRobot[robot[key]] = key;
+  }
+
+  // Return an object with replaced keys with values
+  return normRobot;
 }
 
 module.exports = inverseRobot;
