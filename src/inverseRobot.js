042261ebbbbbc785d756c2b4a6835b78f8e9d9f0
Igor Kashchenko <blackangryrabbit@gmail.com>
2022-12-25 14:35:45 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const checkArr = Object.values(robot);
+  const res = {};
+
+  if (checkArr.length !== new Set(checkArr).size) {
+    return null;
+  }
+
+  for (const key in robot) {
+    res[robot[key]] = key;
+  }
+
+  return res;
 }
 
 module.exports = inverseRobot;
