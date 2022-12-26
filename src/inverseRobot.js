ff3dd47f7e6baa3dad7548901634a5023e9fe309
oleh <shankar.oleh@gmail.com>
2022-12-26 11:28:57 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const invertedCopy = {};
+  const values = Object.values(robot);
+
+  for (let i = 0; i < values.length; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+      if (values[i] === values[j]) {
+        return null;
+      }
+    }
+  }
+
+  for (const key in robot) {
+    invertedCopy[robot[key]] = key + '';
+  }
+
+  return invertedCopy;
 }
 
 module.exports = inverseRobot;
