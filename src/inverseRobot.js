7b2f3397b4a239d6b0ebf5a319c7e217913b2002
Ihor Mariash <imariash86@gmail.com>
2022-12-26 15:22:36 +0200

    code was fixed acording to the checklist

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,6 +6,7 @@
  * @return {object}
 */
 
+/*
 function inverseRobot(robot) {
   // write code here
   const repair = {};
@@ -18,6 +19,23 @@ function inverseRobot(robot) {
   }
 
   return repair;
+}
+*/
+
+function inverseRobot(robot) {
+  // write code here
+  const repaired = {};
+
+  for (const key in robot) {
+    const robotValue = robot[key];
+
+    if (repaired.hasOwnProperty(robotValue)) {
+      return null;
+    }
+    repaired[robotValue] = key;
+  }
+
+  return repaired;
 }
 
 module.exports = inverseRobot;
