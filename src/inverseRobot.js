60bbc323b5a9497fdf68575bcff7522fe6dfa550
karaedd <karaed.ml@mail.ru>
2022-12-27 15:54:23 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,12 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
-}
+  const inverse = Object.fromEntries(Object.entries(robot).map((
+    [key, value]) => [value, key]));
+
+  return Object.keys(
+    inverse).length < Object.values(
+    robot).length ? null : inverse;
+};
 
 module.exports = inverseRobot;
