7ed1ce6ac15f8d26207802a25789833ccaf80b65
Oleksandr Kardash <sashonchic@gmail.com>
2022-12-27 16:21:02 +0200

    Checklist

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,7 +10,7 @@ function inverseRobot(robot) {
   const rotateRobot = {};
 
   for (const [key, value] of Object.entries(robot)) {
-    if (key in robot) {
+    if (value in rotateRobot) {
       return null;
     }
 
