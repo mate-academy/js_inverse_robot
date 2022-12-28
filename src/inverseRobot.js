c98e5881d7dc050cb177edaa9e7024b8eabe9398
Olya Zaiets <zayets.olga@gmail.com>
2022-12-28 21:47:45 +0100

    inverseRobot were implemented

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const correctRobot = {};
+
+  for (const keys in robot) {
+    const value = robot[keys];
+
+    if (correctRobot.hasOwnProperty(value)) {
+      return null;
+    }
+    correctRobot[value] = keys;
+  }
+
+  return correctRobot;
 }
 
 module.exports = inverseRobot;
