9cd22010c2883fa8e43d8b92c51d8809181592d2
Yuliia Andrieieva <andrieieva.y@gmail.com>
2022-12-28 15:11:28 +0200

    Fixed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,18 +8,12 @@
 
 function inverseRobot(robot) {
   const newRobot = {};
-  const arrKeys = Object.keys(robot);
-  const arrValues = Object.values(robot);
 
-  for (let i = 0; i < arrKeys.length; i++) {
-    const newRobotValues = arrKeys[i];
-    const newRobotKeys = arrValues[i];
-
-    if (newRobot.hasOwnProperty(newRobotKeys)) {
+  for (let key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
       return null;
     }
-
-    newRobot[newRobotKeys] = newRobotValues;
+    newRobot[robot[key]] = key;
   }
 
   return newRobot;
