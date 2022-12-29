754c3f1a3233db0fec904b8b1f454032f81a760d
Alessandra Nitti <alessandranitti@MacBook-Air-di-Alessandra.local>
2022-12-29 11:35:10 +0100

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,29 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const duplicateElem = {};
+
+  for (const key in robot) {
+    const robotKey = robot[key];
+
+    if (duplicateElem.hasOwnProperty(robotKey)) {
+      return null;
+    }
+
+    duplicateElem[robot[key]] = key;
+  }
+
+  return duplicateElem;
+
+  // const keysFrom = Object.entries(robot);
+
+  // for (let i = 0; i < keysFrom.length; i++) {
+  //   keysFrom[i].reverse(keysFrom);
+  // }
+
+  // const newObject = Object.fromEntries(keysFrom);
+
+  // return newObject;
 }
 
 module.exports = inverseRobot;
