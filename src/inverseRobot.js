9dd2fba74c3114abaa3b5a5e1890c1c9effd4c90
Ihor Mariash <imariash86@gmail.com>
2022-12-29 08:21:31 +0200

    comments were removed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,22 +6,6 @@
  * @return {object}
 */
 
-/*
-function inverseRobot(robot) {
-  // write code here
-  const repair = {};
-
-  for (const key in robot) {
-    if (repair[robot[key]]) {
-      return null;
-    }
-    repair[robot[key]] = key;
-  }
-
-  return repair;
-}
-*/
-
 function inverseRobot(robot) {
   // write code here
   const repaired = {};
