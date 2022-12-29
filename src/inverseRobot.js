f3d5f2f99a06f8e3b33369dc789a378b64fb0a8b
Mykola Serbin <serbin.nick@gmail.com>
2022-12-29 20:50:33 +0100

    inverse robot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,32 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.keys(robot);
+  const values = Object.values(robot);
+
+  let count = 0;
+
+  for (let i = 0; i < values.length; i++) {
+    if (count > 1) {
+      return null;
+    }
+
+    count = 0;
+
+    for (let j = 0; j < values.length; j++) {
+      if (values[i] === values[j]) {
+        count++;
+      }
+    }
+  }
+
+  const obj = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    obj[values[i]] = keys[i];
+  }
+
+  return obj;
 }
 
 module.exports = inverseRobot;
