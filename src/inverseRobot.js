259c3893beeeceee40b28cd05cba80a9a909ac11
bart6man <igor.musson.55@gmail.com>
2022-12-30 13:40:08 +0200

    Fixed redurant code!

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,15 +9,12 @@
 function inverseRobot(robot) {
   // write code here
   const newRobot = {};
-  const robotValues = Object.values(robot);
-
-  for (const char of robotValues) {
-    if (robotValues.indexOf(char) !== robotValues.lastIndexOf(char)) {
-      return null;
-    }
-  }
 
   for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
     newRobot[robot[key]] = key;
   }
 
