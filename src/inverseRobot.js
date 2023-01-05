8044b17acc5e876c1b67ad17cd9a905b614c14a5
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-05 14:08:35 +0200

    Finish task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,28 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
   // write code here
+  const robotKeys = [];
+  const robotValues = [];
+  const reversedRobot = {};
+
+  for (const key in robot) {
+    robotKeys.push(key);
+    robotValues.push(robot[key]);
+  }
+
+  if (robotValues.length !== new Set(robotValues).size) {
+    return null;
+  }
+
+  for (let i = 0; i < robotKeys.length; i++) {
+    reversedRobot[robotValues[i]] = robotKeys[i];
+  }
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
