8d09de01d4fba78a0fe57c25a5d1392dfcb60f78
Lerimunia <ia.lerimund@gmail.com>
2023-01-05 21:42:44 +0100

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,21 @@
 
 function inverseRobot(robot) {
   // write code here
+  const array = Object.values(robot);
+  const isValuesAreDuplicated = array.some(item => {
+    if (array.indexOf(item) !== array.lastIndexOf(item)) {
+      return true;
+    }
+  });
+  const newRobot = Object.entries(robot).map(
+    ([key, value]) => ({ [value]: key })
+  );
+
+  if (isValuesAreDuplicated) {
+    return null;
+  }
+
+  return Object.assign({}, ...newRobot);
 }
 
 module.exports = inverseRobot;
