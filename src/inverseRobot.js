faed037195cac2d6bd1a005d7b67687c9620c944
Edgar Kocuba <edgarkocuba@gmail.com>
2023-01-07 12:47:40 +0200

    solution inverse robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,21 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inverseArr = {};
+  const robotValues = Object.values(robot);
+  const robotKeys = Object.keys(robot);
+
+  for (let i = 0; i < robotValues.length; i++) {
+    inverseArr[robotValues[i]] = robotKeys[i];
+  }
+
+  const inverseArrKeys = Object.keys(inverseArr);
+
+  if (robotKeys.length !== inverseArrKeys.length) {
+    return null;
+  }
+
+  return inverseArr;
 }
 
 module.exports = inverseRobot;
