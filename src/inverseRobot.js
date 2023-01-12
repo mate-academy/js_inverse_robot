7cf6069e8636f5847b26be1021f1abf301f2782c
Vlad <bojko.vladislav16@gmail.com>
2023-01-12 11:10:03 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const values = Object.values(robot);
+  // Check if we have any repeated value
+  for (let i = 0; i < values.length; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+      if (values[i] === values[j]) {
+        return null;
+      }
+    }
+  }
+
+// return inversed object
+  return Object.keys(robot).reduce((ret, key) => {
+    ret[robot[key]] = key;
+    
+    return ret;
+  }, {});
 }
 
 module.exports = inverseRobot;
