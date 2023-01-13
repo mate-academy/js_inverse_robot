1265f1980d43154cd15e893bf2be8bd515446625
Vlad <bojko.vladislav16@gmail.com>
2023-01-13 12:41:48 +0200

    Solution was optimized

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,19 +8,16 @@
 
 function inverseRobot(robot) {
   const values = Object.values(robot);
+
   // Check if we have any repeated value
-  for (let i = 0; i < values.length; i++) {
-    for (let j = i + 1; j < values.length; j++) {
-      if (values[i] === values[j]) {
-        return null;
-      }
-    }
+  if (values.some((item, index) => values.indexOf(item) !== index)) {
+    return null;
   }
 
-// return inversed object
+  // return inversed object
   return Object.keys(robot).reduce((ret, key) => {
     ret[robot[key]] = key;
-    
+
     return ret;
   }, {});
 }
