a5013bcba226451a99741c28ef9e9e57188325f9
Peti51 <galpeter31@gmail.com>
2023-01-13 14:49:50 +0100

    commit message

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.keys(robot);
+
+  for (let i = 0; i <= keys.length; i++) {
+    for (let j = i + 1; j <= keys.length; j++) {
+      if (robot[keys[i]] === robot[keys[j]]) {
+        return null;
+      }
+    }
+  }
+
+  const result = {};
+
+  for (const key in robot) {
+    result[robot[key]] = key;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
