3b75f327f4fd2f3789ff404d2a96c83bb95b2a16
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-01-14 13:32:51 -0600

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    if (newRobot[robot[key]]) {
+      return null;
+    } // I don't understand how this part works, I found solution
+    // in internet, but still can't explain how
+    // *if* works without condition
+
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
