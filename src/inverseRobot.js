24e556f43a46df5ca93d25cc5a705fa1412fd11a
Mykola Serbin <serbin.nick@gmail.com>
2023-01-15 20:29:57 +0100

    no nesting anymore

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,25 +10,15 @@ function inverseRobot(robot) {
   const keys = Object.keys(robot);
   const values = Object.values(robot);
 
-  let count = 0;
-
-  for (let i = 0; i < values.length; i++) {
-    if (count > 1) {
-      return null;
-    }
-
-    count = 0;
-
-    for (let j = 0; j < values.length; j++) {
-      if (values[i] === values[j]) {
-        count++;
-      }
-    }
-  }
-
   const obj = {};
 
   for (let i = 0; i < keys.length; i++) {
+    const robotKey = values[i];
+
+    if (obj.hasOwnProperty(robotKey)) {
+      return null;
+    }
+
     obj[values[i]] = keys[i];
   }
 
