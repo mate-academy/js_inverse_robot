7050c94502055f92589b2bdc982b46b33958729b
Andrii Khomiak <andr.hamster@gmail.com>
2023-01-15 11:10:53 +0200

    task done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const obj = {};
+
+  for (const key in robot) {
+    if (robot[key] in obj) {
+      return null;
+    }
+
+    obj[robot[key]] = key;
+  }
+
+  return obj;
 }
 
 module.exports = inverseRobot;
