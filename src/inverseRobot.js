6bb1ad3ac78826b6baaa7436b4849191cfa3a17c
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-01-16 18:13:42 +0200

    Task complete

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const swapped = {};
+
+  for (const key in robot) {
+    if (swapped.hasOwnProperty(robot[key])) {
+      return null;
+    } else {
+      swapped[robot[key]] = key;
+    }
+  }
+
+  return swapped;
 }
 
 module.exports = inverseRobot;
