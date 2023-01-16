747fbcd9e8bcad13cde05392083f37709d9b97a8
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-01-16 15:04:28 -0600

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -12,10 +12,7 @@ function inverseRobot(robot) {
   for (const key in robot) {
     if (newRobot[robot[key]]) {
       return null;
-    } // I don't understand how this part works, I found solution
-    // in internet, but still can't explain how
-    // *if* works without condition
-
+    }
     newRobot[robot[key]] = key;
   }
 
