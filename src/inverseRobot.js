f80d530052518c8215cbc8735834c183b42d5181
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-01-20 21:03:50 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const repairedRobot = {};
+  const arr = [];
+
+  for (const key in robot) {
+    arr.push(robot[key]);
+  }
+
+  if (new Set(arr).size !== arr.length) {
+    return null;
+  } else {
+    for (const key in robot) {
+      repairedRobot[robot[key]] = key;
+    }
+  }
+
+  return repairedRobot;
 }
 
 module.exports = inverseRobot;
