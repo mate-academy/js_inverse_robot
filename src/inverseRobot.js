6dc3ca71cb72658d54c584aa4ddcf667e5c71752
Vasyl Pavlenko <genlus@ukr.net>
2023-01-24 10:34:50 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
+ * 1.Object.entries transform object to array
+ * 2.map >= reversed key and value and it's still array
+ * 3.Object.fromEntries transform array back to object
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inversed
+    = Object.fromEntries(Object.entries(robot).map(([key, value]) =>
+      [value, key]));
+
+  return Object.keys(inversed).length
+    < Object.values(robot).length
+    ? null
+    : inversed;
 }
 
 module.exports = inverseRobot;
