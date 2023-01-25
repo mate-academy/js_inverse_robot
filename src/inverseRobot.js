24786b33557e21f55013a32c662303ef083ca3c3
Dmytro Somik <dmytro.somik@mccann.com>
2023-01-25 21:00:59 +0000

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,22 @@
 
 function inverseRobot(robot) {
   // write code here
+  const keys = Object.values(robot);
+  const duplicateElements = keys.filter(
+    (item, index) => keys.indexOf(item) !== index
+  );
+
+  if (duplicateElements.length > 0) {
+    return null;
+  }
+
+  const inversedRobot = {};
+
+  Object.keys(robot).forEach((value, index) => {
+    inversedRobot[keys[index]] = value;
+  });
+
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
