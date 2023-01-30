5108c7ce44d00c9141e636656300e36f6cd31e7d
Vitaly Maistrenko <maystrenko977@gmail.com>
2023-01-30 16:40:59 +0200

    first trye

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -2,12 +2,24 @@
 
 /*
  * @param {object} robot
- *
+ *Створіть функцію «inverseRobot»,
+  яка приймає «robot» як параметр і
+  повертає новий об’єкт, у якому ключі
+   змінюватимуться місцями зі значеннями.
  * @return {object}
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const copyRobot = {};
+
+  for (const key in robot) {
+    if (robot[key] in copyRobot) {
+      return null;
+    }
+    copyRobot[robot[key]] = key;
+  }
+
+  return copyRobot;
 }
 
 module.exports = inverseRobot;
