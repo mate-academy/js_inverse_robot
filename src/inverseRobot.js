23347da6a35c6efd022e36f2328502897b9bf834
Mykola-Borodavka <k_borodavka@ukr.net>
2023-01-30 20:22:19 +0200

    corrections

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,7 +10,7 @@ function inverseRobot(robot) {
   const newRobot = {};
 
   for (const key in robot) {
-    if (newRobot[robot[key]]) {
+    if (newRobot.hasOwnProperty(robot[key])) {
       return null;
     }
 
