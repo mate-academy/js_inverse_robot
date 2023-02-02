0e3dbfeebeb58b370ce5de9b217c64a62f182bca
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2023-02-02 18:57:28 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,25 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const repairedProperties = [];
+  const robotEntries = Object.entries(robot);
+
+  for (let entry of robotEntries) {
+    entry = entry.reverse().map((n) => `${n}`);
+
+    const key = entry[0];
+
+    if (repairedProperties.includes(key)) {
+      return null;
+    } else {
+      repairedProperties.push(key);
+    }
+  }
+
+  return Object.fromEntries(robotEntries);
 }
 
 module.exports = inverseRobot;
