1013112cf861a58cf383938784b74774be219d6d
Zavalishyn Oleksandr <zavalishinalexanderdn@gmail.com>
2023-02-02 17:36:06 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const revert = {};
+  const exclusiveNames = [];
+
+  for (const key in robot) {
+    if (exclusiveNames.includes(robot[key])) {
+      return null;
+    }
+    exclusiveNames.push(robot[key]);
+    revert[robot[key]] = key;
+  }
+
+  return revert;
 }
 
 module.exports = inverseRobot;
