3d6f041dd9408bf5800263cdc0169e9eef737af4
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-02-03 03:14:28 +0200

    inverse_robot - first commit

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,31 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotValues = Object.values(robot);
+
+  for (let i = 0; i < robotValues.length; i++) {
+    for (let j = 0; j < robotValues.length; j++) {
+      if (i !== j) {
+        if (robotValues[i] === robotValues[j]) {
+          return null;
+        }
+      }
+    }
+  }
+
+  const robotEntries = Object.entries(robot);
+  const objectReversed = {};
+
+  for (let i = 0; i < robotEntries.length; i++) {
+    const innerArray = robotEntries[i].reverse();
+
+    const objectKey = innerArray[0];
+    const objectValue = innerArray[1];
+
+    objectReversed[objectKey] = objectValue;
+  }
+
+  return objectReversed;
 }
 
 module.exports = inverseRobot;
