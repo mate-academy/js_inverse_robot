747442c26793e164a577de22e40a1f800cf2dfcb
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-02-09 18:24:55 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inverseObj = {};
+    for(var key in robot) {
+      inverseObj[robot[key]] = key;
+    }
+
+    if (Object.keys(inverseObj).length < Object.values(robot).length) {
+
+     return null;
+    }
+
+    return inverseObj;
 }
 
 module.exports = inverseRobot;
