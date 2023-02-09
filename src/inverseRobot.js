bc789e4b0b6d6dc87a806ef2cdf9391cc1296e38
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-02-09 22:49:29 +0100

    fixed solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -13,9 +13,8 @@ function inverseRobot(robot) {
   for (const entry of entries) {
     if (newRobot[entry[1]]) {
       return null;
-    } else {
-      newRobot[entry[1]] = entry[0];
     }
+    newRobot[entry[1]] = entry[0];
   }
 
   return newRobot;
