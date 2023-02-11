99ccc65b09b0d1cd15f0b5d2e1002ea84078129a
Tolik Petras <toliketras4@gmail.com>
2023-02-11 11:16:46 +0200

    Solution fixed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,24 +7,13 @@
 */
 
 function inverseRobot(robot) {
-  const keys = Object.values(robot);
-  const params = Object.keys(robot);
   const newRobot = {};
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = i + 1; j < keys.length; j++) {
-      if (keys[i] === keys[j]) {
-        return null;
-      } else {
-        let z = 0;
-
-        for (; z < keys.length; z++) {
-          const key = keys[z];
-          const param = params[z];
-
-          newRobot[key] = param;
-        }
-      }
+  for (const key of Object.keys(robot)) {
+    if (newRobot[robot[key]]) {
+      return null;
+    } else {
+      newRobot[robot[key]] = key;
     }
   }
 
