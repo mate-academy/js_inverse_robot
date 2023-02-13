b3776f723415b68aefdd9cbdea06b25f1a81a259
Maksym <maksymmaliuk98@gmail.com>
2023-02-13 17:29:32 +0200

    inverseRobot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+  const key = Object.keys(robot);
+  const value = Object.values(robot);
+
+  for (let i = 0; i < key.length; i++) {
+    newRobot[value[i]] = key[i];
+
+    if (value[i] === value[i + 1]) {
+      return null;
+    }
+
+    if (value.includes(value[i], i + 1)) {
+      return null;
+    }
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
