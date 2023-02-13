db30c45fb3f9d56a43488e75d40bc6e1e9722ab9
Yaroslav Dudar <dudar.komod@gmail.com>
2023-02-13 20:49:21 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,30 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+  const keys = Object.values(robot);
+  const values = Object.keys(robot);
+  let count = 0;
+
+  for (const key of keys) {
+    for (let i = 0; i < keys.length; i++) {
+      if (key === keys[i]) {
+        count++;
+      }
+    }
+
+    if (count > 1) {
+      return null;
+    }
+
+    count = 0;
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    result[keys[i]] = values[i];
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
