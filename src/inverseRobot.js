964dca7e84c862de1cad408f634c474d12858427
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-02-13 21:00:47 +0200

    Was changed solution :)

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,16 +8,9 @@
 
 function inverseRobot(robot) {
   const copy = {};
-  const arrKey = [];
-  const arrVal = [];
 
   for (const key in robot) {
-    arrKey.unshift(key);
-    arrVal.unshift(robot[key]);
-  }
-
-  for (let i = 0; i < arrKey.length; i++) {
-    copy[arrVal[i]] = arrKey[i];
+    copy[robot[key]] = key;
   }
 
   if (Object.keys(robot).length !== Object.keys(copy).length) {
