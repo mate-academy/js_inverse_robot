ff231962cf1e4e2a8bc0d06976b30bcf8fe64918
vadym4che <vadym4che@gmail.com>
2023-02-14 08:15:32 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,8 +6,22 @@
  * @return {object}
 */
 
+// const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
+// const robert = { Robert: 'name', 123: 'chipVer', 113: 'chipVer' };
+// inverseRobot(robert) === null
+// inverseRobot(kolli) === { name: 'Kolli', chipVer: '123', wheels: '3' }
 function inverseRobot(robot) {
-  // write code here
-}
+  const tobor = {};
 
+  Object.keys(robot).forEach(key => {
+    // console.log('key =', key, ', robot[key] =', robot[key]);
+    if (!tobor[robot[key]]) {
+      tobor[robot[key]] = key;
+    }
+  });
+
+  return Object.keys(robot).length === Object.keys(tobor).length ? tobor : null;
+}
+// console.log(inverseRobot(kolli));
+// console.log(inverseRobot(robert));
 module.exports = inverseRobot;
