8dd96c41bfb8894f948a5b45c93253ea71807a27
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-02-15 09:19:27 +0200

    change cycle

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,21 +7,14 @@
  */
 
 function inverseRobot(robot) {
-  // write code here
-  const key = Object.keys(robot);
-  const values = Object.values(robot);
   const reverse = {};
 
-  for (let i = 0; i < key.length; i++) {
-    reverse[values[i]] = key[i];
-
-    if (values[i] === values[i + 1]) {
+  for (const key in robot) {
+    if (reverse.hasOwnProperty(robot[key])) {
       return null;
     }
 
-    if (values.includes(values[i], i + 1)) {
-      return null;
-    }
+    reverse[robot[key]] = key;
   }
 
   return reverse;
