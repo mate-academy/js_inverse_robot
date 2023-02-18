0db6a609abe0326ef5dcc9cc7073a769d03b6ff7
Yaroslav Dudar <dudar.komod@gmail.com>
2023-02-18 17:38:28 +0200

    Fixed some bugs

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,26 +8,12 @@
 
 function inverseRobot(robot) {
   const result = {};
-  const keys = Object.values(robot);
-  const values = Object.keys(robot);
-  let count = 0;
 
-  for (const key of keys) {
-    for (let i = 0; i < keys.length; i++) {
-      if (key === keys[i]) {
-        count++;
-      }
-    }
-
-    if (count > 1) {
+  for (const key in robot) {
+    if (result[robot[key]]) {
       return null;
     }
-
-    count = 0;
-  }
-
-  for (let i = 0; i < keys.length; i++) {
-    result[keys[i]] = values[i];
+    result[robot[key]] = key;
   }
 
   return result;
