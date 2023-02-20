49d50136f71565fa97c51123660bb6867e3ef045
vadym4che <vadym4che@gmail.com>
2023-02-20 02:57:48 +0200

    removed unnessecary comments + added 1 const for readability

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,22 +6,18 @@
  * @return {object}
 */
 
-// const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
-// const robert = { Robert: 'name', 123: 'chipVer', 113: 'chipVer' };
-// inverseRobot(robert) === null
-// inverseRobot(kolli) === { name: 'Kolli', chipVer: '123', wheels: '3' }
 function inverseRobot(robot) {
   const tobor = {};
 
   Object.keys(robot).forEach(key => {
-    // console.log('key =', key, ', robot[key] =', robot[key]);
-    if (!tobor[robot[key]]) {
-      tobor[robot[key]] = key;
+    const sourceKey = robot[key];
+
+    if (!tobor[sourceKey]) {
+      tobor[sourceKey] = key;
     }
   });
 
   return Object.keys(robot).length === Object.keys(tobor).length ? tobor : null;
 }
-// console.log(inverseRobot(kolli));
-// console.log(inverseRobot(robert));
+
 module.exports = inverseRobot;
