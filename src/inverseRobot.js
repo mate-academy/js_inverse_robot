7a71bcb1d3e6f9d11d7ab74174075bb23f320fb7
ctk12 <chetankatwa12@gmail.com>
2023-02-20 16:31:34 +0530

    edit Solution for Inverse Robot after code review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,23 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  const keys = Object.keys(robot);
-  const values = Object.values(robot);
-  const newOb = {};
+  const newObj = {};
 
-  for (let i = 0; i < values.length - 1; i++) {
-    for (let j = i + 1; j < values.length; j++) {
-      if (values[i] === values[j]) {
-        return null;
-      }
+  for (const key in robot) {
+    if (newObj[robot[key]]) {
+      return null;
     }
+
+    newObj[robot[key]] = key;
   }
 
-  for (let k = 0; k < values.length; k++) {
-    newOb[`${values[k]}`] = keys[k];
-  }
-
-  return newOb;
+  return newObj;
 }
 
 module.exports = inverseRobot;
