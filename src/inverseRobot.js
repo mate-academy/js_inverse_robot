4da3765a469627de02b182a8e25b17a0125ef435
ctk12 <chetankatwa12@gmail.com>
2023-02-20 15:59:24 +0530

    Solution for Inverse Robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.keys(robot);
+  const values = Object.values(robot);
+  const newOb = {};
+
+  for (let i = 0; i < values.length - 1; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+      if (values[i] === values[j]) {
+        return null;
+      }
+    }
+  }
+
+  for (let k = 0; k < values.length; k++) {
+    newOb[`${values[k]}`] = keys[k];
+  }
+
+  return newOb;
 }
 
 module.exports = inverseRobot;
