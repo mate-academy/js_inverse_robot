1869c92a22e16f9680a8f246f158c09ee15468c5
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-02-20 10:55:58 +0200

    inverseRobot was created

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const TRUE_KEYS = Object.values(robot);
+  const TRUE_VALUES = Object.keys(robot);
+  const OBJ = {};
+
+  for (let i = 1; i < TRUE_KEYS.length; i++) {
+    let FIRST_KEY = 0;
+
+    if (TRUE_KEYS[FIRST_KEY] === TRUE_KEYS[i]) {
+      return null;
+    }
+
+    FIRST_KEY++;
+  }
+
+  for (let y = 0; y < TRUE_KEYS.length; y++) {
+    OBJ[TRUE_KEYS[y]] = TRUE_VALUES[y];
+  }
+
+  return OBJ;
 }
 
 module.exports = inverseRobot;
