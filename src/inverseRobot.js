139f858d0c74c72c7c458cae9bb4bbb14f595347
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-02-20 22:04:02 +0200

    first update

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,22 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  const TRUE_KEYS = Object.values(robot);
-  const TRUE_VALUES = Object.keys(robot);
+  const TRUE_KEYS = [];
   const OBJ = {};
 
-  for (let i = 1; i < TRUE_KEYS.length; i++) {
-    let FIRST_KEY = 0;
+  for (const [key, value] of Object.entries(robot)) {
+    OBJ[value] = key;
+    TRUE_KEYS.push(value);
 
-    if (TRUE_KEYS[FIRST_KEY] === TRUE_KEYS[i]) {
-      return null;
+    for (let i = 1; i < TRUE_KEYS.length; i++) {
+      let FIRST_KEY = 0;
+
+      if (TRUE_KEYS[FIRST_KEY] === TRUE_KEYS[i]) {
+        return null;
+      }
+
+      FIRST_KEY++;
     }
-
-    FIRST_KEY++;
-  }
-
-  for (let y = 0; y < TRUE_KEYS.length; y++) {
-    OBJ[TRUE_KEYS[y]] = TRUE_VALUES[y];
   }
 
   return OBJ;
