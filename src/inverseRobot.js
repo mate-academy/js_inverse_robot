6ae1a2ca714acefbeeaf39b6060685044be7918b
Oleksandr Lytovchenko <jaster93him@gmail.com>
2023-02-22 01:29:30 +0200

    changed keys and contents

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,26 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const testRepeat = [];
+  const value = [];
+  const resalt = {};
+
+  for (const key in robot) {
+    testRepeat.push(robot[key]);
+    value.push(key);
+  }
+
+  for (let i = 0; i < testRepeat.length - 1; i++) {
+    if (testRepeat.lastIndexOf(testRepeat[i]) !== i) {
+      return null;
+    }
+  }
+
+  for (let j = 0; j < value.length; j++) {
+    resalt[testRepeat[j]] = value[j];
+  }
+
+  return resalt;
 }
 
 module.exports = inverseRobot;
