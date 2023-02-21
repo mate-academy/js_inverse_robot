a33a5b2ae4c539b7e384c707f003cfbe28e76e0c
Maindloll <valeriy.mikheyev@gmail.com>
2023-02-21 18:36:53 +0200

    fixed coderewiev

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -11,7 +11,7 @@ function inverseRobot(robot) {
   const newRobot = {};
 
   for (const key in robot) {
-    if (newRobot[robot[key]]) {
+    if (newRobot.hasOwnProperty([robot[key]])) {
       return null;
     } else {
       newRobot[robot[key]] = key;
