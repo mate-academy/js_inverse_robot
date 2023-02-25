381e428eed0f0c914083b63d7609282dd527b961
Julia <julia@ocuhealthcare.com>
2023-02-25 14:04:40 -0600

    Solution for Situs inversus

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    if (newRobot[robot[key]]) {
+      return null;
+    }
+
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
