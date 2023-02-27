10f7482323b7c2c181fc7d723de7492377e2b025
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-02-27 16:15:10 +0100

    complete task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = Object.entries(robot).map(([a, b]) => [b, a]);
+
+  for (let i = 0; i < newRobot.length - 1; i++) {
+    if (newRobot[0][0] === newRobot[i + 1][0]) {
+      return null;
+    }
+  }
+
+  const end = Object.fromEntries(newRobot);
+
+  return end;
 }
 
 module.exports = inverseRobot;
