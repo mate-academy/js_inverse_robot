78992659b9ab660bad5b3464e5b2ae0b4fa700ef
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-02-27 20:15:34 +0100

    fix task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,14 +7,17 @@
  */
 
 function inverseRobot(robot) {
-  const newRobot = Object.entries(robot).map(([a, b]) => [b, a]);
+  const keys = Object.keys(robot);
 
-  for (let i = 0; i < newRobot.length - 1; i++) {
-    if (newRobot[0][0] === newRobot[i + 1][0]) {
-      return null;
+  for (let i = 0; i < keys.length; i++) {
+    for (let j = i + 1; j < keys.length; j++) {
+      if (robot[keys[i]] === robot[keys[j]]) {
+        return null;
+      }
     }
   }
 
+  const newRobot = Object.entries(robot).map(([a, b]) => [b, a]);
   const end = Object.fromEntries(newRobot);
 
   return end;
