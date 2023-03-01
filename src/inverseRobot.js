18bb958c574eb68396cb18d2f0679cdaa3a47615
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-03-01 11:34:39 +0100

    Fixes after code review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,18 +7,12 @@
 */
 
 function inverseRobot(robot) {
-  const values = Object.values(robot);
   const inverse = {};
 
-  for (let i = 0; i < values.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (values[j] === values[i]) {
-        return null;
-      }
-    }
-  }
-
   for (const key in robot) {
+    if (robot[key] in inverse) {
+      return null;
+    }
     inverse[robot[key]] = key;
   }
 
