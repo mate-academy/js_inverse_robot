31529795c9c54b508b95bd21152784ec78ed08cc
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-03-04 12:09:22 +0200

    fixes

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,16 +1,10 @@
 'use strict';
 
-/*
- * @param {object} robot
- *
- * @return {object}
-*/
-
 function inverseRobot(robot) {
   const inverse = {};
 
   for (const [key, value] of Object.entries(robot)) {
-    if (inverse[value] !== undefined) {
+    if (inverse[value]) {
       return null;
     }
 
