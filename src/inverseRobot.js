4b91b157dc443a01b733a4362c5a22a354e2acd6
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-03-04 14:08:28 +0200

    first version

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -12,7 +12,7 @@ function inverseRobot(robot) {
   for (const key in robot) {
     const value = robot[key];
 
-    if (result[value] !== undefined) {
+    if (result.hasOwnProperty(value)) {
       return null;
     }
     result[value] = key;
