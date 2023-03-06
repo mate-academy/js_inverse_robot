14d1bc6b65f12ce853380474ca24fa563514a56e
Viktor <vasha2020@gmail.com>
2023-03-06 15:51:01 +0000

    first commit

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  let newKey;
+  let newValue;
+
+  for (const key in robot) {
+    newKey = robot[key];
+    newValue = key;
+    delete robot[key];
+    robot[newKey] = newValue;
+  }
+
+  return robot;
 }
 
 module.exports = inverseRobot;
