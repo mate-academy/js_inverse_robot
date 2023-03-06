124d1a1a00284c1927edad375c16c2f6ccc566bb
Antonina Klishch <tonya91t@gmail.com>
2023-03-06 12:16:37 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+  let newKey = '';
+
+  for (const key in robot) {
+    newKey = robot[key];
+
+    if (newKey in result) {
+      return null;
+    } else {
+      result[newKey] = key;
+    }
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
