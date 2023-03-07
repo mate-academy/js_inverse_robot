b49f279d89ef6e9e6082c53ae28b61365e4468be
domkab <dominykas.bobinas@gmail.com>
2023-03-07 21:46:58 +0200

    solution before review

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,33 @@
 'use strict';
 
-/*
- * @param {object} robot
- *
- * @return {object}
-*/
+const robert = {
+  Robert: 'name',
+  123: 'chipVer',
+  113: 'chipVerPro',
+};
+
+inverseRobot(robert);
 
 function inverseRobot(robot) {
-  // write code here
+  const inversedRobot = {};
+
+  if (robot.hasOwnProperty('name') && robot.name !== 'name') {
+    inversedRobot[robot.name] = 'name';
+  }
+
+  for (const objectKey in robot) {
+    const value = robot[objectKey];
+
+    if (objectKey !== 'name') {
+      if (Object.keys(inversedRobot).includes(value)) {
+        return null;
+      }
+    }
+
+    inversedRobot[value] = objectKey;
+  }
+
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
