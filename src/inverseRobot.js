6dd7427209c79bbdf85ccf50ccab1f952ae68eb7
Pindel Kateryna <katpin1400@gmail.com>
2023-03-09 17:08:01 +0100

    new solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,30 +9,16 @@
 function inverseRobot(robot) {
   // write code here
   const inversedRobot = {};
-  const arrKeys = [];
-  let repeat = false;
 
   for (const key in robot) {
-    arrKeys.push(robot[key]);
-  }
-
-  for (let i = 0; i < arrKeys.length; i++) {
-    for (let j = i + 1; j < arrKeys.length; j++) {
-      if (arrKeys[i] === arrKeys[j]) {
-        repeat = true;
-      }
-    }
-  }
-
-  if (repeat === false) {
-    for (const key in robot) {
+    if (inversedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    } else {
       inversedRobot[robot[key]] = key;
     }
-
-    return (inversedRobot);
-  } else {
-    return (null);
   }
+
+  return (inversedRobot);
 }
 
 module.exports = inverseRobot;
