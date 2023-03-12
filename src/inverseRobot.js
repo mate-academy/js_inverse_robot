3ba8c9d3de62fee92decdb17c715bfaa40478edb
Maryna Oliinyk <ukrmartin22@gmail.com>
2023-03-12 19:41:04 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const repaired = {};
+
+  const values = Object.values(robot);
+  const unique = new Set(values);
+
+  if (values.length !== unique.size) {
+    return null;
+  }
+
+  for (const key in robot) {
+    repaired[robot[key]] = key;
+  }
+
+  return repaired;
 }
 
 module.exports = inverseRobot;
