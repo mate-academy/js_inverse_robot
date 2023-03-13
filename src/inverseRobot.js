a059b3343db0317878c680a0f276fa2b2f0cd4df
Artem Kyrylenko <kyrylenko.a.a@gmail.com>
2023-03-13 15:39:04 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,8 +6,23 @@
  * @return {object}
 */
 
+// const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
+// const robert = { Robert: 'name', 123: 'chipVer', 113: 'chipVer' };
+// inverseRobot(robert) === null
+// inverseRobot(kolli) === { name: 'Kolli', chipVer: '123', wheels: '3' }
+
 function inverseRobot(robot) {
-  // write code here
+  const invRobot = {};
+
+  for (const key in robot) {
+    if (invRobot[robot[key]]) {
+      return null;
+    } else {
+      invRobot[robot[key]] = key;
+    }
+  }
+
+  return invRobot;
 }
 
 module.exports = inverseRobot;
