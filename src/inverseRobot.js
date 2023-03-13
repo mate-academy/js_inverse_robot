7a0f2bfe63aef8670489dd4e226d816430d2101d
Artem Kyrylenko <kyrylenko.a.a@gmail.com>
2023-03-13 15:40:54 +0200

    Deleted comments

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,11 +6,6 @@
  * @return {object}
 */
 
-// const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
-// const robert = { Robert: 'name', 123: 'chipVer', 113: 'chipVer' };
-// inverseRobot(robert) === null
-// inverseRobot(kolli) === { name: 'Kolli', chipVer: '123', wheels: '3' }
-
 function inverseRobot(robot) {
   const invRobot = {};
 
