e4f756224db4f813dad40c050f694f0082d5adcb
Mark Boiko <boikomark555@gmail.com>
2023-03-13 17:34:57 +0200

    create inverseRobot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
-}
+  const arrOfKeys = Object.values(robot).reverse();
+  const arrOfValue = Object.keys(robot).reverse();
+  const inverseObjectRobot = {};
 
+  const lenOfArrays = arrOfKeys.length;
+
+  for (let i = 0; i < lenOfArrays; i++) {
+    if (inverseObjectRobot[arrOfKeys[i]]) {
+      return null;
+    }
+    inverseObjectRobot[arrOfKeys[i]] = arrOfValue[i];
+  }
+
+  return inverseObjectRobot;
+}
 module.exports = inverseRobot;
