cb88d7c4621768d03315a874f5203311b24ed0be
Oleh Kolodii <bigknife4@gmail.com>
2023-03-13 20:21:42 +0200

    made a solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,22 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newRobot = {};
+  const robotEntries = Object.entries(robot);
+
+  for (let i = 0; i < robotEntries.length; i++) {
+    for (let j = i; j < robotEntries.length; j++) {
+      if (robotEntries[i][1] === robotEntries[j][1] && i !== j) {
+        return null;
+      }
+    }
+  }
+
+  for (const entity of robotEntries) {
+    newRobot[entity[1]] = entity[0];
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
