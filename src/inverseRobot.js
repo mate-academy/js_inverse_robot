e16987f2097ffa5443db3af1bca0f203be1040bb
Anna Latusha <annapop03072701@gmail.com>
2023-03-13 16:37:12 +0200

    Fixed robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,16 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inversRobot = {};
+
+  for (const key in robot) {
+    if (inversRobot[robot[key]]) {
+      return null;
+    }
+    inversRobot[robot[key]] = key;
+  }
+
+  return inversRobot;
 }
 
 module.exports = inverseRobot;
