44520c2b5739bdf4176ce50f7e981f7d278fd350
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-03-14 22:13:46 +0300

    fix checklist

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,11 +10,14 @@ function inverseRobot(robot) {
   const clearSetting = {};
 
   for (const key in robot) {
-    if (clearSetting[robot[key]] !== undefined) {
+    const rightKey = robot[key];
+    const rightValue = key;
+
+    if (clearSetting.hasOwnProperty(rightKey)) {
       return null;
     }
 
-    clearSetting[robot[key]] = `${key}`;
+    clearSetting[rightKey] = `${rightValue}`;
   }
 
   return clearSetting;
