68d85280239aa3803bdb03d408e61ccfb68df22c
Vladyslav <vladlischenovsky@gmail.com>
2023-03-14 17:24:29 +0200

    add fixed style

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -12,7 +12,7 @@ function inverseRobot(robot) {
   for (const key in robot) {
     const value = robot[key];
 
-    if (reverseRobot[value] !== undefined) {
+    if (reverseRobot.hasOwnProperty(value)) {
       return null;
     }
     reverseRobot[value] = key;
