76de6df5cd10cd79ec570d416f010fb74e7ddacc
Nazarii Chepil <nazzikk1998@gmail.com>
2023-03-14 19:53:05 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,21 @@
 
 function inverseRobot(robot) {
   // write code here
+  const trueRobot = {};
+  const params = [];
+
+  for (const key in robot) {
+    const paramName = robot[key];
+
+    if (paramName === params.find(val => val === paramName)) {
+      return null;
+    }
+    params.push(paramName);
+
+    trueRobot[paramName] = key;
+  }
+
+  return trueRobot;
 }
 
 module.exports = inverseRobot;
