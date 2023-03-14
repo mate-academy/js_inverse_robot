425838f0f2cfcdfa9a815d869a03827d871d4d37
Anastasiia Yuzyfyshyn <nastyajuzufichn@gmail.com>
2023-03-14 13:28:57 +0200

    inverse robot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotValues = Object.values(robot);
+  const firstRobotValue = robotValues[0];
+
+  for (let i = 1; i < robotValues.length; i++) {
+    if (robotValues[i] === firstRobotValue) {
+      return null;
+    }
+  }
+
+  const swapedRobot = Object.entries(robot).map(
+    ([key, value]) => [value, key]
+  );
+
+  return Object.fromEntries(swapedRobot);
 }
 
 module.exports = inverseRobot;
