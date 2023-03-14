d770c5dd5fd533be489830fd4a62420e353d8157
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-03-14 23:29:37 +0200

    inversed robots

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,25 @@
-'use strict';
+"use strict";
 
 /*
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
   // write code here
+  const result = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (result[value] !== undefined) {
+      return null;
+    }
+    result[value] = key;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
