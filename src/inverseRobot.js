569f9dc4c5dec1a68412c7881e1d514ca3579ab5
Alex Rog <sasharg303@gmail.com>
2023-03-15 21:24:24 +0100

    version1

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,24 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotValuesMas = Object.values(robot);
+  const originalRobotValues = [];
+
+  for (const curr of robotValuesMas) {
+    if (originalRobotValues.includes(curr)) {
+      return null;
+    }
+
+    originalRobotValues.push(curr);
+  }
+
+  const changedObj = {};
+
+  for (const key in robot) {
+    changedObj[robot[key]] = key;
+  }
+
+  return changedObj;
 }
 
 module.exports = inverseRobot;
