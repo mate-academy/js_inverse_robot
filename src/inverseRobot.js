3823c9c4fe89644cc741505e3b2b8c2196687e8f
Andrii <remote.makandriival@gmail.com>
2023-03-15 11:38:35 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newRobot = {};
+
+  for (const elem in robot) {
+    if (robot[elem] in newRobot) {
+      return null;
+    } else {
+      newRobot[robot[elem]] = elem;
+    }
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
