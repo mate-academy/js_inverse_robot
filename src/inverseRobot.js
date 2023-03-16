1230b1195bd1b8b5a80af9efae5819363c70c384
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-16 12:42:22 +0100

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  let array = [];
+
+  // Change object into one array
+  Object.entries(robot).forEach(par => {
+    array = array.concat(par);
+  });
+
+  // check if array constains duplicates
+  const checkDuplicates = (new Set(array)).size !== array.length;
+
+  if (checkDuplicates) {
+    return null;
+  }
+
+  // make arrays from objects keys and swap them;
+  const fixRobot = Object.entries(robot).map(([key, value]) => [value, key]);
+
+  // change arrays into object and return
+  return Object.fromEntries(fixRobot);
 }
 
 module.exports = inverseRobot;
