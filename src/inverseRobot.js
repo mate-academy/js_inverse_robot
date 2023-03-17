12578fd9f03bbc2cc517306521cd6ecca1b4a8f0
Tolik Serhiychuk <sergiytchoukk@gmail.com>
2023-03-17 11:54:44 +0200

    used correct check for property presence in object

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,7 +4,7 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+//  */
 
 function inverseRobot(robot) {
   const reversedRobot = {};
@@ -12,7 +12,7 @@ function inverseRobot(robot) {
   for (const key in robot) {
     const value = robot[key];
 
-    if (reversedRobot[value] !== undefined) {
+    if (reversedRobot.hasOwnProperty(value)) {
       return null;
     }
     reversedRobot[value] = key;
