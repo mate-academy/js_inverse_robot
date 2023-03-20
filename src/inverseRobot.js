ddc8835bda18e71e9f7bec53a7655d30ad264997
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-20 23:32:42 +0200

    commit

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,12 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inversedRobot  = {};
+
+  for (const[key, value] of Object.entries(robot)) {
+    inversedRobot[value] = key;
+  }
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
