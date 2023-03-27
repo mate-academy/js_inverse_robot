132d59df7cb9301011f2692b447a30f05018b723
AndriiYelieva <andrii.yelieva@gmail.com>
2023-03-27 23:35:45 +0300

    good?

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,22 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotNew = {};
+  const values = Object.values(robot);
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (values.indexOf(value) !== values.lastIndexOf(value)) {
+      return null;
+    }
+    robotNew[value] = key;
+  }
+
+  return robotNew;
 }
 
 module.exports = inverseRobot;
