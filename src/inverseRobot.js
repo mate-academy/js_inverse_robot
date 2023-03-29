14f61385b05c8bb2841da62793b7f08d82b93a18
Natalia Klonowska <natalia742@gmail.com>
2023-03-29 19:26:56 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,20 @@
 
 function inverseRobot(robot) {
   // write code here
+  const fixRobot = {};
+
+  for (const key in robot) {
+    const fixKey = robot[key];
+    const fixValue = key;
+
+    if (fixRobot.hasOwnProperty(fixKey)) {
+      return null;
+    }
+
+    fixRobot[fixKey] = fixValue;
+  }
+
+  return fixRobot;
 }
 
 module.exports = inverseRobot;
