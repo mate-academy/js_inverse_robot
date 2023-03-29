1345ba8764295e41e0c48c97a2bdbc4d8056b9a2
kq333 <afugowski@yandex.com>
2023-03-29 15:01:03 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,32 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  if (Object.keys(robot).length === 0) {
+    return {};
+  }
+
+  const arrayItems = [];
+
+  const reversedEntries = Object.entries(robot).map((entry) => {
+    const [key, value] = entry;
+
+    if (arrayItems.includes(value)) {
+      return null;
+    }
+
+    arrayItems.push(value);
+
+    return [value, key];
+  });
+
+  if (reversedEntries.includes(null)) {
+    return null;
+  }
+
+  return Object.fromEntries(reversedEntries);
 }
 
 module.exports = inverseRobot;
