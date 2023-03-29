dd1e12f5c7cf25928eda075539b57aa1d8f08035
Atlas Werecik <mw.greybeard72@gmail.com>
2023-03-29 21:16:39 +0200

    task solution approach 29/03/2023  21:16

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const swappedRobot = {};
+
+  for (const key in robot) {
+    if (swappedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    } else {
+      swappedRobot[robot[key]] = key;
+    }
+  }
+
+  return swappedRobot;
 }
 
 module.exports = inverseRobot;
