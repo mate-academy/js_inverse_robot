9d295ae913aca2bdbcb92b65ed39d5f713506113
sylcym <prusak.syl@gmail.com>
2023-03-29 18:55:15 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const transponsedRobot = {};
+
+  for (const [key, value] of Object.entries(robot)) {
+    transponsedRobot[value] = key;
+  }
+
+  if (Object.keys(robot).length !== Object.keys(transponsedRobot).length) {
+    return null;
+  }
+
+  return transponsedRobot;
 }
 
 module.exports = inverseRobot;
