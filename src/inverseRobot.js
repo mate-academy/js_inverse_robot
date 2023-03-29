cdbb2dab3a51f0b8cc9c931377f5d9c5d2b21be7
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-03-29 16:57:29 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robotKeys = Object.keys(robot);
+  const robotValues = Object.values(robot);
+
+  for (let i = 0; i < robotValues.length; i++) {
+    if (robotValues.indexOf(robotValues[i])
+    !== robotValues.lastIndexOf(robotValues[i])) {
+      return null;
+    }
+  }
+
+  const newRobot = {};
+
+  for (let j = 0; j < robotValues.length; j++) {
+    for (let k = j; k < robotKeys.length; k++) {
+      newRobot[robotValues[j]] = robotKeys[j];
+    }
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
