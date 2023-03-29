b21084e179b46c21cd548ac461152fbff0f9ab45
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-03-29 15:23:52 +0200

    solution v1

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const robotToFix = robot;
+  const fixedRobot = {};
+
+  for (const part in robotToFix) {
+    if (fixedRobot.hasOwnProperty(robotToFix[part])) {
+      return null;
+    }
+    fixedRobot[robotToFix[part]] = part;
+  }
+
+  return (fixedRobot);
 }
 
 module.exports = inverseRobot;
