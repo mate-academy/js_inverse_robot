dc6358cb4dcdda2517f482fce0406cdb9b4d26fe
PufAnimalsDev <93289768+PufAnimalsDev@users.noreply.github.com>
2023-03-30 07:49:45 +0200

    add solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,22 @@
 
 function inverseRobot(robot) {
   // write code here
+  const robotValues = Object.values(robot);
+  const invertedRobot = {};
+
+  for (let i = 0; i < robotValues.length; i++) {
+    const checkValue = robotValues.shift();
+
+    if (robotValues.includes(checkValue)) {
+      return null;
+    }
+  }
+
+  for (const key in robot) {
+    invertedRobot[robot[key]] = key;
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
