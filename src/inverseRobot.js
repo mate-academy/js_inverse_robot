c4a2463921e4ba3bbf9ab593fb110852eefe6e6f
Anastasiia Vorobets <anastasiiavorobets03@gmail.com>
2023-03-31 19:07:31 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,21 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newRobot = {};
+  const duplicates = [];
+
+  for (const key in robot) {
+    duplicates.push(robot[key]);
+
+    for (let i = 0; i < duplicates.length - 1; i++) {
+      if (duplicates[i] === robot[key]) {
+        return null;
+      }
+    }
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
