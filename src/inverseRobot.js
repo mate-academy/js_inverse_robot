1c6195ff90084c9ec61fc885f75f84b21e4ff58b
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-04-04 15:02:08 +0200

    code simplified

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,16 +10,12 @@ function inverseRobot(robot) {
   const robotKeys = Object.keys(robot);
   const robotValues = Object.values(robot);
 
-  for (let i = 0; i < robotValues.length; i++) {
-    if (robotValues.indexOf(robotValues[i])
-    !== robotValues.lastIndexOf(robotValues[i])) {
-      return null;
-    }
-  }
-
   const newRobot = {};
 
   for (let j = 0; j < robotValues.length; j++) {
+    if (newRobot.hasOwnProperty(robotValues[j])) {
+      return null;
+    }
     newRobot[robotValues[j]] = robotKeys[j];
   }
 
