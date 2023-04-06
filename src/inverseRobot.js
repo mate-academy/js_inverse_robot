ba26cbd98f9cac9768c1b5c8b7e0eb1576fe87c2
Illya Myhalyk <ilamihalik@gmail.com>
2023-04-06 22:21:16 +0300

    getting the reverse order of pairs of values ​​from an object

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const robot2 = {};
+
+  for (const key in robot) {
+    if (robot2.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    robot2[robot[key]] = key;
+  }
+
+  return robot2;
 }
 
 module.exports = inverseRobot;
