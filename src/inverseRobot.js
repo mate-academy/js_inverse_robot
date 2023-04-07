01877258773ba98970c583d9338eb547172406f5
Absolution26 <zet2571428@gmail.com>
2023-04-07 20:19:08 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const objKeys = Object.keys(robot);
+  const objValues = Object.values(robot);
+  const trueRobot = {};
+
+  for (let i = 0; i < objKeys.length; i++) {
+    if (objValues.includes(objValues[i], i + 1)
+    || objKeys.includes(objKeys[i], i + 1)) {
+      return null;
+    } else {
+      trueRobot[objValues[i]] = objKeys[i];
+    }
+  }
+
+  return trueRobot;
 }
 
 module.exports = inverseRobot;
