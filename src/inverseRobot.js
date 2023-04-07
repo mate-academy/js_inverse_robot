7e6dd3a785e82cd044bcc74241e33a7b1e5e83d5
AnastassiaH <nastyagurska@gmail.com>
2023-04-07 21:32:55 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,22 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newRobot = {};
+  const robotValues = Object.values(robot);
+
+  const isSameValue = robotValues.some((item) => {
+    return robotValues.indexOf(item) !== robotValues.lastIndexOf(item);
+  });
+
+  if (isSameValue) {
+    return null;
+  }
+
+  for (const [key, value] of Object.entries(robot)) {
+    newRobot[value] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
