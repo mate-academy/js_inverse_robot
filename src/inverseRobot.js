101626f7762b030c50f0274b42448900e999457e
AnastassiaH <nastyagurska@gmail.com>
2023-04-08 13:21:54 +0300

    optimize code

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,17 +9,11 @@
 function inverseRobot(robot) {
   // write code here
   const newRobot = {};
-  const robotValues = Object.values(robot);
-
-  const isSameValue = robotValues.some((item) => {
-    return robotValues.indexOf(item) !== robotValues.lastIndexOf(item);
-  });
-
-  if (isSameValue) {
-    return null;
-  }
 
   for (const [key, value] of Object.entries(robot)) {
+    if (newRobot[value]) {
+      return null;
+    }
     newRobot[value] = key;
   }
 
