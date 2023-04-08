842332f8f818e4b3271bab6ada9bae71ad533eae
Ratibor R <classicmedical2@gmail.com>
2023-04-08 17:08:47 +0300

    Solution #2

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,13 +9,12 @@
 function inverseRobot(robot) {
   // write code here
   const newRobot = {};
-  const seenKeys = {};
 
   for (const key in robot) {
-    if (seenKeys.hasOwnProperty(robot[key])) {
+    if (newRobot.hasOwnProperty(robot[key])) {
       return null;
     }
-    seenKeys[robot[key]] = true;
+
     newRobot[robot[key]] = key;
   }
 
