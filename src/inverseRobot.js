2546e1fc60b07882baaf533663b0815ff3c1c85d
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2023-04-09 23:06:09 +0300

    Inverted Robot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,21 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+
+  for (const key in robot) {
+    if (key in robot) {
+      const value = robot[key];
+
+      if (invertedRobot[value] !== undefined) {
+        return null;
+      }
+
+      invertedRobot[value] = key;
+    }
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
