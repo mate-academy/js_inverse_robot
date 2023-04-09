5e058aa653286a2c58e0e43baad24ad0877e97eb
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-09 12:21:32 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,28 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const entries = Object.entries(robot);
+  const vals = Object.values(robot);
+
+  if (!isDuplicated(vals)) {
+    const entriesToObj = entries.map(el => el.reverse());
+    const obj = Object.fromEntries(entriesToObj);
+
+    return obj;
+  }
+
+  return null;
 }
+
+// checking for duplicated values in array
+function isDuplicated(tab) {
+  for (let i = 0; i < tab.length; i++) {
+    if (tab.indexOf(tab[i]) !== i) {
+      return true;
+    };
+  }
+
+  return false;
+};
 
 module.exports = inverseRobot;
