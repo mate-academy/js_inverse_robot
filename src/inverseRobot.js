0e2ac2025224402a0d8cd558cb6d67f48b1d5e76
Yuliia Nudyk <yulyayu833@gmail.com>
2023-04-12 03:19:02 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,29 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  let robotInverse = {};
+
+  for (const [newValue, newKey] of Object.entries(robot)) {
+    if (checkRepeats(newKey, newValue)) {
+      robotInverse = null;
+
+      return robotInverse;
+    } else {
+      robotInverse[newKey] = newValue;
+    }
+  }
+
+  function checkRepeats(currentKey, currentValue) {
+    for (const [key, value] of Object.entries(robotInverse)) {
+      if (currentKey === key || currentValue === value) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  return robotInverse;
 }
 
 module.exports = inverseRobot;
