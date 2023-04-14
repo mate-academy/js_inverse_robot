62f1efe41fd5bf7767b3776da545bcbc64bd25aa
Iryna Khalniepiesova <karolinkagrey@gmail.com>
2023-04-14 14:47:28 +0300

    First commit

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,28 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+
+  const arrKeys = [];
+
+  for (const key in robot) {
+    arrKeys.push(key);
+  }
+
+  const arrValues = [];
+
+  for (const key in robot) {
+    arrValues.push(robot[key]);
+  }
+
+  for (let i = 0; i < arrKeys.length; i++) {
+    if (arrValues[i] in result) {
+      return null;
+    }
+    result[arrValues[i]] = arrKeys[i];
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
