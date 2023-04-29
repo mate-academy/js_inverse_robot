263b965263a87be810727d44093d8c6901d44c3d
Dmytro Yevtyeryev <begoodeee@gmail.com>
2023-04-29 10:26:31 +0300

    Fixed requested changes

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,22 +9,12 @@
 function inverseRobot(robot) {
   const fixedRobot = {};
 
-  const keys = Object.keys(robot);
-
-  const values = Object.values(robot);
-
-  for (let i = 0; i < keys.length; i++) {
-    const checkKeys = keys.indexOf(keys[i], i + 1);
-
-    const checkValues = values.indexOf(values[i], i + 1);
-
-    if (checkKeys > -1 || checkValues > -1) {
+  for (const key in robot) {
+    if (fixedRobot.hasOwnProperty(robot[key])) {
       return null;
     }
-  }
 
-  for (let i = 0; i < values.length; i++) {
-    fixedRobot[values[i]] = keys[i];
+    fixedRobot[robot[key]] = key;
   }
 
   return fixedRobot;
