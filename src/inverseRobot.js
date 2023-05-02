9a81830608204d2dc760212d631097b36d23e6a1
Diana Tuz <dianatuz12@gmail.com>
2023-05-02 23:19:21 +0300

    task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (newRobot[value] !== undefined) {
+      return null;
+    }
+
+    newRobot[value] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
