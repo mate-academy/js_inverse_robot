fa3f993d202f698411975487fcb6a30c52124622
Oleksii Hrytsenko <oleksii.hrytsenko@icloud.com>
2023-05-03 09:49:08 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+
+  for (const robotKey in robot) {
+    const robotValue = robot[robotKey];
+
+    if (result[robotValue] !== undefined) {
+      return null;
+    }
+
+    result[robotValue] = robotKey;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
