44f67dcedf97aacc9cc92c9d30d2536bbe1e6c50
Andrii <zisuan.od@gmail.com>
2023-05-04 13:56:57 +0300

    task done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,21 @@
 
 function inverseRobot(robot) {
   // write code here
+  const robotValues = Object.values(robot).sort();
+
+  for (let i = 1; robotValues.length > i; i++) {
+    if (robotValues[i] === robotValues[i - 1]) {
+      return null;
+    }
+  }
+
+  const result = {};
+
+  Object.entries(robot).forEach(([key, value]) => {
+    result[value] = key;
+  });
+
+  return result;
 }
 
 module.exports = inverseRobot;
