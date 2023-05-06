862817552db495e9d234f3df98baeb6e79750c15
George Savostikov <georgesavostikov@gmail.com>
2023-05-06 22:12:51 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  let newRobot = {};
+
+  const maxElement = Object.values(robot);
+
+  for (let i = 0; i < maxElement.length; i++) {
+    for (let j = i + 1; j < maxElement.length; j++) {
+      if (maxElement[i] === maxElement[j]) {
+        newRobot = null;
+
+        return newRobot;
+      }
+    }
+  }
+
+  for (const key in robot) {
+    newRobot[robot[key]] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
