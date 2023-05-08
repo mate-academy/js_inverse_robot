597978dfc00626b5ad5a73592731957e887a51e9
AlexKherson <q0992358472@gmail.com>
2023-05-08 15:18:44 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,22 @@
 
 function inverseRobot(robot) {
   // write code here
+  const result = {};
+  const valueList = [];
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (valueList.includes(value)) {
+      return null;
+    }
+
+    valueList.push(value);
+
+    result[value] = key;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
