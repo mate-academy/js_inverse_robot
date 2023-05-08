7ab4436c725c314cf33d699d39bf144c30832f9a
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-08 14:39:49 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inverse = {};
+
+  for (const key in robot) {
+    if (inverse.hasOwnProperty(robot[key])) {
+      return null;
+    }
+    inverse[robot[key]] = key;
+  }
+
+  return inverse;
 }
+
+// inverseRobot({
+//   Robert: 'name', 123: 'chipVer', 113: 'chipVer',
+// });
 
 module.exports = inverseRobot;
