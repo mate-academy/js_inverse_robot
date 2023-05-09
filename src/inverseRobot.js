54e5e4c4fbaf38626febd3645360176269709e25
Iryna Balandiukh <balandiukh.i@gmail.com>
2023-05-09 16:19:48 +0300

    Add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const reversedRobot = {};
+
+  for (const key in robot) {
+    const robotKey = robot[key];
+
+    if (reversedRobot.hasOwnProperty(robotKey)) {
+      return null;
+    }
+
+    reversedRobot[robotKey] = key;
+  }
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
