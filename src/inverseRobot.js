f1b54ad66e95e41b093d466f668330c839219073
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-09 22:48:22 +0300

    change variable name

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,20 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  const result = {};
+  const inverseObject = {};
 
   for (const key in robot) {
-    result[robot[key]] = key;
+    inverseObject[robot[key]] = key;
   }
 
   const countKeyPassedObject = Object.keys(robot).length;
-  const countKeyResult = Object.keys(result).length;
+  const countKeyResult = Object.keys(inverseObject).length;
 
   if (countKeyPassedObject > countKeyResult) {
     return null;
   }
 
-  return result;
+  return inverseObject;
 }
 
 module.exports = inverseRobot;
