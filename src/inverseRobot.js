4cd184fa0534145889d7476add8ef23cad5c79c6
Anton Cherepinets <manickmage@ukr.net>
2023-05-09 15:51:41 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    const newKey = robot[key];
+
+    if (newRobot[newKey]) {
+      return null;
+    }
+    newRobot[newKey] = key;
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
