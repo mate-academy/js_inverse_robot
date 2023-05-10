3cf1febfa47c90480995efd6da404208eb34975a
Roksolana Tylipska <roksolana1407@gmail.com>
2023-05-10 16:07:16 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const repeatedRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (repeatedRobot.hasOwnProperty(value)) {
+      return null;
+    }
+
+    repeatedRobot[value] = key;
+  }
+
+  return repeatedRobot;
 }
+
+inverseRobot({
+  Kolli: 'name', 123: 'chipVer', 3: 'wheels',
+});
 
 module.exports = inverseRobot;
