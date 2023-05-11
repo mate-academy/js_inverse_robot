ba10e69c664e34fff84f908551938172ffc49708
Sonya Liubchenko <conilub@gmail.com>
2023-05-11 11:54:54 +0300

    new solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,23 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  const fixedRobot = {};
-  const values = Object.keys(robot);
-  const keys = Object.values(robot);
+  const noTranspositionRobot = {};
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let j = i + 1; j < keys.length; j++) {
-      if (keys[i] === keys[j]) {
+  for (const key in robot) {
+    if (robot.hasOwnProperty(key)) {
+      const value = robot[key];
+
+      if (noTranspositionRobot.hasOwnProperty(value)) {
         return null;
       }
+      noTranspositionRobot[value] = key;
     }
   }
 
-  for (let i = 0; i < keys.length; i++) {
-    fixedRobot[keys[i]] = values[i];
-  }
-
-  return fixedRobot;
+  return noTranspositionRobot;
 }
 
 module.exports = inverseRobot;
