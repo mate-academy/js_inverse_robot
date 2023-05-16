80774e07b6ccc6f4d9d8437df20926909d55ad04
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-16 12:58:12 +0300

    add solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,29 @@
-'use strict';
+/* eslint-disable quotes */
+"use strict";
 
 /*
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
   // write code here
+  const res = {};
+  const valuesArr = Object.values(robot);
+  const isValuesRepeated = valuesArr.some(
+    (item, index) => valuesArr.indexOf(item) !== index
+  );
+
+  if (isValuesRepeated) {
+    return null;
+  }
+
+  for (const key in robot) {
+    res[robot[key]] = key;
+  }
+
+  return res;
 }
 
 module.exports = inverseRobot;
