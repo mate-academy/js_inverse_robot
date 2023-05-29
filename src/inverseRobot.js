25b2bf2e0875a94b9294f581be1c38821a69bb70
Serhii <gashysh@protonmail.com>
2023-05-29 03:04:34 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+  const usedKeywords = [];
+
+  for (const key in robot) {
+    if (!usedKeywords.includes(key) && !usedKeywords.includes(robot[key])) {
+      invertedRobot[robot[key]] = key;
+      usedKeywords.push(key);
+      usedKeywords.push(robot[key]);
+    } else {
+      return null;
+    }
+  }
+
+  return invertedRobot;
 }
+
+inverseRobot();
 
 module.exports = inverseRobot;
