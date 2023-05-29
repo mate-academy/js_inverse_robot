7c32c168af2d006b8c92567f9425988fbef0ee79
Serhii <gashysh@protonmail.com>
2023-05-29 17:47:42 +0300

    comments was removed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,14 +1,7 @@
 'use strict';
 
-/*
- * @param {object} robot
- *
- * @return {object}
-*/
-
 function inverseRobot(robot) {
   const invertedRobot = {};
-  // const usedKeywords = [];
 
   for (const key in robot) {
     if (invertedRobot.hasOwnProperty(robot[key])) {
