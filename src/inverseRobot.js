5c659d7930c44aaf91af34ffac48fd1355512eaa
grinjk1993 <grinjk1993@gmail.com>
2023-06-04 20:00:33 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const ROBOT_VALUES = Object.values(robot);
+
+  function findDuplicates(arry) {
+    return arry.filter((item, index) => arry.indexOf(item) !== index);
+  };
+
+  const DUP_VALUES = findDuplicates(ROBOT_VALUES);
+
+  if (DUP_VALUES.length >= 1) {
+    return null;
+  }
+
+  return Object.fromEntries(
+    Object
+      .entries(robot)
+      .map(([key, value]) => [value, key])
+  );
 }
 
 module.exports = inverseRobot;
