8a7391b003a19cb091158fda7cc7d0bf4b2908ea
Oleksii Bielovolov <whiteoxofficial@gmail.com>
2023-06-04 16:42:27 +0300

    inverseRobot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,33 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+  const massKeys = dublicate(Object.keys(robot));
+  const massValues = dublicate(Object.values(robot));
+
+  if (massKeys || massValues === null) {
+    return null;
+  }
+
+  for (const key in robot) {
+    result[robot[key]] = key;
+  }
+
+  return result;
+}
+
+function dublicate(mass) {
+  const result = {};
+
+  mass.forEach(element => {
+    result[element] = (result[element] || 0) + 1;
+  });
+
+  for (const key in result) {
+    if (result[key] >= 2) {
+      return null;
+    }
+  }
 }
 
 module.exports = inverseRobot;
