7a337532f1fb7f88adfb7d23636e445643522813
Roman Tarasov <rtarasov1k90@gmail.com>
2023-06-05 12:12:41 +0300

    inversed

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+  const duplicateCheck = Object.values(robot);
+
+  for (let i = 0; i < duplicateCheck.length; i++) {
+    for (let j = i + 1; j < duplicateCheck.length; j++) {
+      if (duplicateCheck[i] === duplicateCheck[j]) {
+        return null;
+      }
+    }
+  }
+
+  for (const key in robot) {
+    result[robot[key]] = key;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
