d79dca6435bc48199c69905359c7c616144ef201
grinjk1993 <grinjk1993@gmail.com>
2023-06-05 10:38:01 +0300

    fixed after comments

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,16 +6,16 @@
  * @return {object}
 */
 
+function findDuplicates(array) {
+  return array.filter((item, index) => array.indexOf(item) !== index);
+};
+
 function inverseRobot(robot) {
-  const ROBOT_VALUES = Object.values(robot);
+  const robotValues = Object.values(robot);
 
-  function findDuplicates(arry) {
-    return arry.filter((item, index) => arry.indexOf(item) !== index);
-  };
+  const duplicateValues = findDuplicates(robotValues);
 
-  const DUP_VALUES = findDuplicates(ROBOT_VALUES);
-
-  if (DUP_VALUES.length >= 1) {
+  if (duplicateValues.length) {
     return null;
   }
 
