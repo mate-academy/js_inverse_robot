9141442a54b390f4ce9afb7d1db21acebbc4203b
Yehor Korablin <yegor_ka2004@ukr.net>
2023-06-08 21:02:30 +0300

    Added check to repeated keys

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,22 +7,16 @@
 */
 
 function inverseRobot(robot) {
-  const keys = [];
-
-  for (const key in robot) {
-    keys.push(robot[key]);
-  }
-
-  for (let i = 0; i < keys.length; i++) {
-    if (keys.includes(keys[i], i + 1)) {
-      return null;
-    }
-  }
-
   const inverse = {};
 
   for (const key in robot) {
-    inverse[robot[key]] = key;
+    const inverseKey = robot[key];
+
+    if (inverse.hasOwnProperty(inverseKey)) {
+      return null;
+    }
+
+    inverse[inverseKey] = key;
   }
 
   return inverse;
