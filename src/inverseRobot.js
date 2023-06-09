702ff63dc9473c6c8b206747fcae83db213449da
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-09 16:20:47 +0300

    Finished inverseRobot task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,15 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const entries = Object.entries(robot).map(([key, value]) => [value, key]);
+  const allElements = entries.flatMap(arr => [...arr]);
+  const filteredArray = [...new Set(allElements)];
+
+  if (filteredArray.length === allElements.length) {
+    return Object.fromEntries(entries);
+  }
+
+  return null;
 }
 
 module.exports = inverseRobot;
