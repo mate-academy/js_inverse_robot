2fb4bce86124f342fb598fac08523228d83b805e
Kristina Bekher <bekherkr@gmail.com>
2023-06-10 20:46:01 +0200

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,19 @@
 
 function inverseRobot(robot) {
   // write code here
+  const newRobot = { };
+  const keys = Object.keys(robot);
+
+  for (let i = 0; i < keys.length; i++) {
+    for (let k = i + 1; k < keys.length; k++) {
+      if (robot[keys[i]] === robot[keys[k]]) {
+        return null;
+      }
+    }
+    newRobot[robot[keys[i]]] = keys[i];
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
