413b2fdb194b9ce0bc696a96ab1d6cec704a340d
Lina Do <dykaya0916@gmail.com>
2023-06-10 23:06:24 +0300

    task solved

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,25 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.values(robot);
+  const values = Object.keys(robot);
+  let value = '';
+  let key = '';
+  const result = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (keys.slice(i + 1).includes(keys[i])) {
+      return null;
+    }
+    key = keys[i];
+    value = values[i];
+    result[key] = value;
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
