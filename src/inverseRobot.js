b6065b5a81b43af1b291bd4164c39d1b2b8ee2a4
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-06-11 15:55:35 +0300

    Task done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,37 @@
-'use strict';
+"use strict";
 
 /*
  * @param {object} robot
  *
  * @return {object}
-*/
-
+ */
 function inverseRobot(robot) {
-  // write code here
+  const keys = Object.values(robot);
+  const obj = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const elem = keys[i];
+
+    if (!obj[elem]) {
+      obj[elem] = 1;
+    } else {
+      obj[elem] += 1;
+    }
+  }
+
+  const values = Object.values(obj).some((item) => item > 1);
+
+  if (values) {
+    return null;
+  }
+
+  const array = Object.fromEntries(
+    Object.entries(robot)
+      .reverse()
+      .map((item) => item.reverse())
+  );
+
+  return array;
 }
 
 module.exports = inverseRobot;
