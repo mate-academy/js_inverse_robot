809e4d2b1ec4f07447d2eae3d95810c14f53edae
Kristina Bekher <bekherkr@gmail.com>
2023-06-11 18:38:32 +0200

    update task solution, remove inner loop

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,17 +7,14 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
   const newRobot = { };
-  const keys = Object.keys(robot);
 
-  for (let i = 0; i < keys.length; i++) {
-    for (let k = i + 1; k < keys.length; k++) {
-      if (robot[keys[i]] === robot[keys[k]]) {
-        return null;
-      }
+  for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
     }
-    newRobot[robot[keys[i]]] = keys[i];
+
+    newRobot[robot[key]] = key;
   }
 
   return newRobot;
