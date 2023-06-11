4b5f5c6027e57ed338667baabbb8c5c5b21fec2e
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-11 11:36:48 +0300

    create inverse Robot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const VALUES_ROBOT = Object.values(robot);
+
+  for (let i = 0; i < VALUES_ROBOT.length; i++) {
+    for (let j = i; j < VALUES_ROBOT.length; j++) {
+      if (VALUES_ROBOT[i] === VALUES_ROBOT[j + 1]) {
+        return null;
+      }
+    }
+  }
+
+  const INVERSE_KEYS_AND_VALUES = {};
+
+  for (const key in robot) {
+    if (robot.hasOwnProperty(key)) {
+      INVERSE_KEYS_AND_VALUES[robot[key]] = key;
+    }
+  }
+
+  return INVERSE_KEYS_AND_VALUES;
 }
 
 module.exports = inverseRobot;
