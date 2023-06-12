c03a694a5b606b4cfd7d972ae5e604795b451796
Artem Andrushchenko <artem.abdrushenko213654@gmail.com>
2023-06-12 16:56:26 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -12,7 +12,7 @@ function inverseRobot(robot) {
   for (const key in robot) {
     const value = robot[key];
 
-    if (invertedRobot[value] !== undefined) {
+    if (invertedRobot.hasOwnProperty(value)) {
       return null;
     }
 
