8eb198800321791c4b23569bd4bfcebb577331ef
david.shamiiev <david.shamiiev@rad4m.com>
2023-06-12 12:09:35 +0300

    fixed solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,12 +10,10 @@ function inverseRobot(robot) {
   const swappedObj = {};
 
   for (const key in robot) {
-    if (robot.hasOwnProperty(key)) {
-      if (swappedObj.hasOwnProperty(robot[key])) {
-        return null;
-      };
-      swappedObj[robot[key]] = key;
-    }
+    if (swappedObj.hasOwnProperty(robot[key])) {
+      return null;
+    };
+    swappedObj[robot[key]] = key;
   }
 
   return swappedObj;
