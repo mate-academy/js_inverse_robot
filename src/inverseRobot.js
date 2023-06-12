b8a2c4af7d92d9dcb2f6b2fcaa70e3e4aa1728d1
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-06-12 16:49:26 +0300

    fixing by checklist

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -13,13 +13,12 @@ function inverseRobot(robot) {
   for (const keys in robot) {
     //  here we need to check
     //  if the resultObject has the same key as our iterator
-    if (resultObject[robot[keys]]) {
+    if (resultObject.hasOwnProperty(robot[keys])) {
       //  if yes, the fuction will return null
       return null;
-    } else {
-      //  if no, the function will write the key and its value
-      resultObject[robot[keys]] = keys;
-    }
+    };
+    //  if no, the function will write the key and its value
+    resultObject[robot[keys]] = keys;
   };
 
   return resultObject;
