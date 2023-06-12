df033454d16e13378b8c921ca612bb25b6c51314
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-06-12 16:39:57 +0300

    added task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const resultObject = {};
+
+  //  now we are iterating the object
+  for (const keys in robot) {
+    //  here we need to check
+    //  if the resultObject has the same key as our iterator
+    if (resultObject[robot[keys]]) {
+      //  if yes, the fuction will return null
+      return null;
+    } else {
+      //  if no, the function will write the key and its value
+      resultObject[robot[keys]] = keys;
+    }
+  };
+
+  return resultObject;
 }
 
 module.exports = inverseRobot;
