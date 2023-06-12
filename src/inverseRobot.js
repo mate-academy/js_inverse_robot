1b5fe4f3e99dbf1e3b756ee0f647faf1d499bf93
Roman Tarasov <rtarasov@gmail.com>
2023-06-12 17:42:35 +0300

    less loops

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,18 +8,13 @@
 
 function inverseRobot(robot) {
   const result = {};
-  const duplicateCheck = Object.values(robot);
-
-  for (let i = 0; i < duplicateCheck.length; i++) {
-    for (let j = i + 1; j < duplicateCheck.length; j++) {
-      if (duplicateCheck[i] === duplicateCheck[j]) {
-        return null;
-      }
-    }
-  }
 
   for (const key in robot) {
-    result[robot[key]] = key;
+    if (!result[robot[key]]) {
+      result[robot[key]] = key;
+    } else {
+      return null;
+    }
   }
 
   return result;
