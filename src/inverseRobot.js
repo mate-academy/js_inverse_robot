22509af106013c42e60c20223e7c91f65bdfe823
Alexey Taranenko <alloha.t.99@gmail.com>
2023-06-12 16:55:48 +0300

    inversed_robot_solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,18 @@
 
 function inverseRobot(robot) {
   // write code here
+  const reapiredRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (value in reapiredRobot) {
+      return null;
+    }
+    reapiredRobot[value] = key;
+  }
+
+  return reapiredRobot;
 }
 
 module.exports = inverseRobot;
