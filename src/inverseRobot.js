e8750aa6df12a803f8528cd5119f461395118840
outofs <rtranscendental@gmail.com>
2023-06-12 12:20:58 +0300

    removing unnecessary check

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -9,16 +9,12 @@
 function inverseRobot(robot) {
   const invRobot = {};
 
-  for (const key in robot) {
-    if (Object.hasOwnProperty.call(robot, key)) {
-      const value = robot[key];
-
-      if (invRobot.hasOwnProperty(value)) {
-        return null;
-      }
-
-      invRobot[value] = key;
+  for (const [key, value] of Object.entries(robot)) {
+    if (invRobot.hasOwnProperty(value)) {
+      return null;
     }
+
+    invRobot[value] = key;
   }
 
   return invRobot;
