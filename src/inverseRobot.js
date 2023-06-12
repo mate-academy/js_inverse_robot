61f931294f5838d2328a4d707c4990f67f027d97
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-12 14:00:58 +0300

    update solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,22 +7,13 @@
 */
 
 function inverseRobot(robot) {
-  const VALUES_ROBOT = Object.values(robot);
-
-  for (let i = 0; i < VALUES_ROBOT.length; i++) {
-    for (let j = i; j < VALUES_ROBOT.length; j++) {
-      if (VALUES_ROBOT[i] === VALUES_ROBOT[j + 1]) {
-        return null;
-      }
-    }
-  }
-
   const INVERSE_KEYS_AND_VALUES = {};
 
   for (const key in robot) {
-    if (robot.hasOwnProperty(key)) {
-      INVERSE_KEYS_AND_VALUES[robot[key]] = key;
+    if (INVERSE_KEYS_AND_VALUES[robot[key]] !== undefined) {
+      return null;
     }
+    INVERSE_KEYS_AND_VALUES[robot[key]] = key;
   }
 
   return INVERSE_KEYS_AND_VALUES;
