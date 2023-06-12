225fa3062b214d1c82b9d1e141776adc1b1baa78
Lina Do <dykaya0916@gmail.com>
2023-06-12 23:03:54 +0300

    updated solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*
  * @param {object} robot
@@ -7,21 +7,13 @@
  */
 
 function inverseRobot(robot) {
-  const keys = Object.values(robot);
-  const values = Object.keys(robot);
-  let value = '';
-  let key = '';
   const result = {};
-
-  for (let i = 0; i < keys.length; i++) {
-    if (keys.slice(i + 1).includes(keys[i])) {
+  for (let key in robot) {
+    if (robot[key] in result) {
       return null;
     }
-    key = keys[i];
-    value = values[i];
-    result[key] = value;
+    result[robot[key]] = key;
   }
-
   return result;
 }
 
