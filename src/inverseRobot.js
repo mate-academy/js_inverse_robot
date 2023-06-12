e68ef367b843293d8feb11fef90d15ad8a1066c1
Pavlo Kuraksin <kuraksinpavel@gmail.com>
2023-06-12 19:02:46 +0300

    add solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+  const robotKeys = Object.values(robot);
+  const robotValues = Object.keys(robot);
+
+  for (let index = 0; index < robotKeys.length; index++) {
+    if (newRobot.hasOwnProperty(robotKeys[index])) {
+      return null;
+    }
+
+    newRobot[robotKeys[index]] = robotValues[index];
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
