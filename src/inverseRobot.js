4b46cc63ac15ef1b294cee5e0420ca0041ac07a9
Sergey Arnautov <sirius9312@ukr.net>
2023-06-13 11:48:03 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,36 @@
 
 function inverseRobot(robot) {
   // write code here
+  const ONE_VALUE_IN_OBJECT = 1;
+  let countsObj = {};
+
+  const resObj = {};
+
+  if (Object.keys(robot).length === 0) {
+    return {};
+  }
+
+  for (const key in robot) {
+    if (countsObj[robot[key]] !== undefined) {
+      countsObj[robot[key]]++;
+    } else {
+      countsObj[robot[key]] = ONE_VALUE_IN_OBJECT;
+    }
+
+    countsObj = Object.assign(countsObj, countsObj[robot[key]]);
+  }
+
+  for (const key in countsObj) {
+    if (countsObj[key] > ONE_VALUE_IN_OBJECT) {
+      return null;
+    } else {
+      for (const key2 in robot) {
+        resObj[robot[key2]] = key2;
+      }
+    }
+  }
+
+  return resObj;
 }
 
 module.exports = inverseRobot;
