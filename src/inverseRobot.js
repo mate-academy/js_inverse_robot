f7986e4fcd276bb7cd7afde3cf009b6a0c183032
Vitalii <fevitaliis@gmail.com>
2023-06-13 13:52:38 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -5,9 +5,34 @@
  *
  * @return {object}
 */
+const kolli = { 
+  Kolli: 'name', 
+  123: 'chipVer', 
+  3: 'wheels' 
+};
+  const robert = { 
+    Robert: 'name', 
+    123: 'chipVer', 
+    113: 'chipVer' 
+  };
+
 
 function inverseRobot(robot) {
-  // write code here
+  const inverseObject = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (inverseObject.hasOwnProperty(value)) {
+      return null;
+    }
+
+    inverseObject[value] = key;
+  }
+
+  return inverseObject;
 }
+
+
 
 module.exports = inverseRobot;
