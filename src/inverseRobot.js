fd2883f30e750e330721b328b2d578971fb0aa3a
Lina Do <dykaya0916@gmail.com>
2023-06-14 10:15:17 +0300

    comments were deleted

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,19 +1,15 @@
 "use strict";
 
-/*
- * @param {object} robot
- *
- * @return {object}
- */
-
 function inverseRobot(robot) {
   const result = {};
-  for (let key in robot) {
+
+  for (const key in robot) {
     if (robot[key] in result) {
       return null;
     }
     result[robot[key]] = key;
   }
+
   return result;
 }
 
