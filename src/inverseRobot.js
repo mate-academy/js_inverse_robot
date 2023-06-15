1445a95c975df9e3e4b91eb24113e2ebad231d77
unknown <fanton81@gmail.com>
2023-06-15 17:10:44 +0300

    mySolution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inverRobot = {};
+
+  for (const key in robot) {
+    if (inverRobot[robot[key]]) {
+      return null;
+    }
+
+    inverRobot[robot[key]] = key;
+  }
+
+  return inverRobot;
 }
 
 module.exports = inverseRobot;
