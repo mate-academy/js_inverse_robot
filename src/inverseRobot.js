91ca50d0a22f06c3ad942bd6b3bdf2e7350efc81
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-06-15 09:47:45 +0200

    implement func inverseRobot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,25 @@
-'use strict';
+"use strict";
 
 /*
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const repairedRobot = {}; // create a new object
+
+  // replace keys and values
+  for (const key in robot) {
+    // check if we meet the key again - return null
+    if (repairedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    } // if we don't - add normal value to normal key
+    repairedRobot[robot[key].toString()] = key;
+  }
+
+  // return a healthy robot :)
+  return repairedRobot;
 }
 
 module.exports = inverseRobot;
