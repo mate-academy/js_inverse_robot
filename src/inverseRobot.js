cd48721b6c6e9e3ce753c10248e18ad33348677e
Andrey Bikir <bikir.a.v@gmail.com>
2023-06-15 23:52:07 +0300

    Inverse object

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const rusultInverse = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+    const chek = rusultInverse.hasOwnProperty([value]);
+
+    if (chek) {
+      return null;
+    }
+
+    rusultInverse[value] = key;
+  }
+
+  return rusultInverse;
 }
 
 module.exports = inverseRobot;
