248a227881fe5dd240344115423acadebecaf8f2
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-06-20 11:36:56 +0200

    add tusk solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,18 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const object = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (object[value] !== undefined) {
+      return null;
+    }
+    object[value] = key;
+  }
+
+  return object;
 }
 
 module.exports = inverseRobot;
