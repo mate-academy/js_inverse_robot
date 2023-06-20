e4d5b699879ae246c798675e937afaf8a5770a8f
Pety Liepikhov <petylep@gmail.com>
2023-06-20 13:37:03 +0300

    finish exercise

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,37 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  // check for same values:
+  const robotsValues = Object.values(robot);
+
+  for (let i = 0; i < robotsValues.length - 1; i++) {
+    const robotValue = robotsValues[i];
+
+    const copyIndex = robotsValues.indexOf(robotValue, i + 1);
+
+    if (copyIndex > -1) {
+      return null;
+    }
+  }
+
+  // revert keys:
+  const reparedRobot = {};
+  const oldKeys = Object.keys(robot);
+
+  for (const oldKey of oldKeys) {
+    const newKey = robot[oldKey];
+
+    // check for empty key:
+    if ((newKey === null)
+        || (newKey === undefined)
+        || (newKey === '')) {
+      return null;
+    }
+
+    reparedRobot[newKey] = oldKey;
+  }
+
+  return reparedRobot;
 }
 
 module.exports = inverseRobot;
