970475999c999db05676ffc68cd16e46445791eb
Anton Bilous <toxanteloxran@gmail.com>
2023-06-26 13:11:44 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+
+  const values = Object.values(robot);
+  const duplicates = values.filter((value, index) =>
+    values.indexOf(value) !== index);
+
+  if (duplicates.length >= 1) {
+    return null;
+  }
+
+  for (const FakeKey in robot) {
+    const key = robot[FakeKey];
+
+    invertedRobot[key] = FakeKey;
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
