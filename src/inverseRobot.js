f17139c93a66a4c1d28200bde8818ee85da9f919
Denis Kononenko <deniskononenko88@gmail.com>
2023-06-27 21:44:26 +0300

    add requested changes

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,16 +8,16 @@
 
 function inverseRobot(robot) {
   // write code here
-  const robotValues = Object.values(robot);
-  const uniqueRobotValues = new Set(robotValues);
   const inversedRobot = {};
 
-  if (robotValues.length !== uniqueRobotValues.size) {
-    return null;
-  }
-
   for (const key in robot) {
-    inversedRobot[robot[key]] = key;
+    const newKey = robot[key];
+
+    if (inversedRobot[newKey]) {
+      return null;
+    }
+
+    inversedRobot[newKey] = key;
   }
 
   return inversedRobot;
