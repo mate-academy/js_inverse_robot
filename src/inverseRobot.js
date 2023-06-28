51225912bfe240262f9ac1cf8f1a6b493c21e879
Viktoria-Moroz <viktoriaandriivna25@gmail.com>
2023-06-28 20:26:44 +0300

    the inverse-robot is done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const changeProperties = {};
+
+  for (const key in robot) {
+    if (changeProperties.hasOwnProperty(key)
+    || changeProperties.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    if (!changeProperties.hasOwnProperty(key)
+    || changeProperties.hasOwnProperty(robot[key])) {
+      changeProperties[robot[key]] = key;
+    }
+  }
+
+  const correctOrder = Object.assign({}, changeProperties);
+
+  return correctOrder;
 }
 
 module.exports = inverseRobot;
