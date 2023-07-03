e51c7ea61e131d48a48d708f49a2bc0f81544c6d
Yaroslav Bilokon <uvill.yarik@gmail.com>
2023-07-03 13:40:35 +0300

    remove coments

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,19 +7,8 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
   const inversRobot = {};
   const ckeck = [];
-
-  // for (let i = 0; i < Object.values(robot).length; i++) {
-  //   if (ckeck.includes(Object.values(robot)[i])) {
-  //     return null;
-  //   };
-
-  //   ckeck.push(Object.values(robot)[i]);
-
-  //   inversRobot[Object.values(robot)[i]] = Object.keys(robot)[i];
-  // }
 
   for (let i = Object.keys(robot).length - 1; i >= 0; i--) {
     if (ckeck.includes(Object.values(robot)[i])) {
@@ -35,10 +24,3 @@ function inverseRobot(robot) {
 };
 
 module.exports = inverseRobot;
-// const str = {
-//   Kolli: 'name',
-//   123: 'chipVer',
-//   3: 'wheels',
-// };
-
-// console.log(inverseRobot(str));
