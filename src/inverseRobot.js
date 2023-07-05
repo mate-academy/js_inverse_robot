c38257ebf243fa63e51c7036bc7261ca6558ba7e
Albert Kifiuk <albertkifiuk@gmail.com>
2023-07-05 14:40:16 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const swapperobot = {};
+  const values = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (values[value] === undefined) {
+      values[value] = key;
+      swapperobot[value] = key;
+    }
+
+    if (values[value] !== key) {
+      return null;
+    }
+  }
+
+  return swapperobot;
 }
 
 module.exports = inverseRobot;
