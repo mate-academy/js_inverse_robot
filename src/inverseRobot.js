65fcd7b611c34d729a52371b84fc5b0b0d4c920d
Danylo-Chumakov <danil26032003@gmail.com>
2023-07-06 17:46:36 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,19 @@
 
 function inverseRobot(robot) {
   // write code here
+  const swappedRobot = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (swappedRobot.hasOwnProperty(value)) {
+      return null;
+    }
+
+    swappedRobot[value] = key;
+  }
+
+  return swappedRobot;
 }
 
 module.exports = inverseRobot;
