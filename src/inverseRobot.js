8222af154672a72da20c048578ec34f46edf3f30
Viktoriia Melnyk <melnychka1993@gmail.com>
2023-07-06 11:58:30 +0100

    task is done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const first = Object.keys(robot);
+  const second = Object.values(robot);
+  const newObj = {};
+
+  for (let i = 0; i < second.length; i++) {
+    for (let j = i + 1; j < second.length; j++) {
+      if (second[i] === second[j]) {
+        return null;
+      }
+    }
+  }
+
+  for (let i = 0; i < first.length; i++) {
+    newObj[second[i]] = first[i];
+  }
+
+  return newObj;
 }
 
 module.exports = inverseRobot;
