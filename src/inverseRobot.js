c9b1e6b311f40a23b8adc7c649924dc0849abf60
Denys Poltorak <itwfrontend@gmail.com>
2023-07-09 18:24:50 +0300

    feat: the solution of the task (Sun, Jul 09, 2023 06:24:49 PM)

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,26 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  // "robot"'s keys become values and values become "robot"'s keys
+  const turnedRobot = {};
+
+  for (const key in robot) {
+    const INCOMING_ROBOT_VALUE = robot[key];
+    const INCOMING_ROBOT_KEY = key;
+
+    // Check if there are already such values in the created object
+    const hasDoubleValue = turnedRobot.hasOwnProperty(INCOMING_ROBOT_VALUE);
+    const hasDoubleKey = turnedRobot.hasOwnProperty(INCOMING_ROBOT_KEY);
+
+    // if turnedRobot has double keys or values, return null
+    if (hasDoubleValue || hasDoubleKey) {
+      return null;
+    }
+
+    turnedRobot[INCOMING_ROBOT_VALUE] = INCOMING_ROBOT_KEY;
+  }
+
+  return turnedRobot;
 }
 
 module.exports = inverseRobot;
