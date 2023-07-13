45c53ff485331fbcfbd79f3102cd51acafb4f6af
Kacper <leman_kacper@o2.pl>
2023-07-13 12:08:23 +0200

    Soultion to task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const result = {};
+  const properties = Object.values(robot);
+  const values = Object.keys(robot);
+
+  for (let i = 0; i < properties.length; i++) {
+    const property = properties[i];
+    const value = values[i];
+
+    if (result[property] === undefined) {
+      result[property] = value;
+    } else {
+      return null;
+    }
+  }
+
+  return result;
 }
 
 module.exports = inverseRobot;
