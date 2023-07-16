1dde9df3173d45e337a6043d5b4ddcea4b8e66fe
Ivan Krylo <ivan.poltaven@gmail.com>
2023-07-16 14:56:02 +0300

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,30 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  // Масив з усіма ключами робота
+  const robotKeys = Object.keys(robot);
+  // Масив з усіма значеннями ключів робота
+  const robotValues = Object.values(robot);
+  // Новий об'єкт для повернення
+  const repairedRobot = {};
+
+  // Перебір всіх індексів масиву зі значеннями
+  for (let i = 0; i < robotValues.length; i++) {
+    // Підстановка в масив -
+    // ключів зі значень 'хворого' робота та значень із ключів.
+    // (На мою думку, мало спрацювати XD)
+    repairedRobot[robotValues[i]] = robotKeys[i];
+
+    // Перебір всіх індексів масиву після "i"
+    for (let j = i + 1; j < robotValues.length; j++) {
+      // Якщо якісь елементи масиву '===' то повертаю null
+      if (robotValues[i] === robotValues[j]) {
+        return null;
+      }
+    }
+  }
+
+  return repairedRobot;
 }
 
 module.exports = inverseRobot;
