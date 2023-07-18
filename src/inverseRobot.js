dba0cec0f7b9b332859d07888e9c877a713667f3
ZakZh <38009936+ZakZh@users.noreply.github.com>
2023-07-18 23:32:29 +0300

    task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,20 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const resultRobot = {};
+
+  for (const [indx, key] of Object.entries(robot)) {
+    if (resultRobot[key]) {
+      return null;
+    }
+
+    resultRobot[key] = indx;
+  }
+
+  return resultRobot;
 }
 
 module.exports = inverseRobot;
