47e4ad095aba2a1fa0246949ee56becb6ad778b5
FurArt <iiionerart@gmail.com>
2023-07-19 08:58:02 +0300

    added solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -2,11 +2,28 @@
 
 /*
  * @param {object} robot
- *
- * @return {object}
+*
+* @return {object}
 */
 
 function inverseRobot(robot) {
+  const newRobot = {};
+  let checkDoubleProper = false;
+
+  Object.entries(robot).forEach((key) => {
+    if (newRobot.hasOwnProperty(key[1])) {
+      checkDoubleProper = null;
+
+      return;
+    }
+    newRobot[key[1]] = key[0];
+  });
+
+  if (checkDoubleProper === null) {
+    return null;
+  }
+
+  return newRobot;
   // write code here
 }
 
