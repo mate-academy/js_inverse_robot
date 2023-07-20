8dd907ff1ad3e1c0edb4d88a0f0d0258385ac003
Vladyslav Korobka <vladseipa@gmail.com>
2023-07-21 00:09:25 +0300

    task has been done

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,22 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+  let isNull = 0;
+
+  Object.entries(robot).forEach(pair => {
+    if (!newRobot[pair[1]]) {
+      newRobot[pair[1]] = pair[0];
+    } else {
+      isNull = 1;
+    }
+  });
+
+  if (isNull === 1) {
+    return null;
+  };
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
