b0f285025c7554c7aae7a77c450c06ebdb0a8c21
MartaChobaniuk <marta.chobaniuk@gmail.com>
2023-07-20 15:39:40 +0300

    function inverseRobot

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const invertedRobot = {};
+  const values = Object.values(robot);
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (values.filter((v) => v === value).length > 1) {
+      return null;
+    }
+
+    invertedRobot[value] = key;
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
