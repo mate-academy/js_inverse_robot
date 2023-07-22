e437633f45bd431a002dff661136b6e4b6a5f285
Vladyslav Kolisnyk <vadyslavkolisnyk20@gmail.com>
2023-07-22 21:47:26 +0300

    add task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const inverseObject = {};
+
+  for (const key in robot) {
+    if (!inverseObject[robot[key]]) {
+      inverseObject[robot[key]] = key;
+    } else {
+      return null;
+    }
+  }
+
+  return inverseObject;
 }
 
 module.exports = inverseRobot;
