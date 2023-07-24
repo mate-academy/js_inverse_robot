b91b9095a33c603973e2bacf9b367a829b78cbcf
kabaktech <kabacznikmobile@gmail.com>
2023-07-24 19:00:17 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,17 @@
 'use strict';
 
-/*
- * @param {object} robot
- *
- * @return {object}
-*/
-
 function inverseRobot(robot) {
-  // write code here
+  const newRobot = {};
+
+  for (const key in robot) {
+    if (newRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    newRobot[robot[key]] = String(key);
+  }
+
+  return newRobot;
 }
 
 module.exports = inverseRobot;
