2658237608eeb37c3cdad8f9b269905d908ea976
Andrii Kuz <andrykuz4556@gmail.com>
2023-07-27 13:14:37 +0300

    Solved task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,17 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inversedRobot = {};
+
+  for (const key in robot) {
+    if (!inversedRobot[robot[key]]) {
+      inversedRobot[robot[key]] = key;
+    } else {
+      return null;
+    }
+  }
+
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
