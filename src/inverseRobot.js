ed3677b9bc26e6ab3db2f28830ad1c9f05ef944d
Szymon Pachucki <szymonpachucki010@gmail.com>
2023-07-27 11:34:24 +0200

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -10,16 +10,14 @@ function inverseRobot(robot) {
   const invertedValues = [];
 
   for (const key in robot) {
-    if (robot.hasOwnProperty(key)) {
-      const value = robot[key];
+    const value = robot[key];
 
-      if (invertedValues.indexOf(value) !== -1) {
-        return null;
-      }
-
-      invertedRobot[value] = key;
-      invertedValues.push(value);
+    if (invertedValues.indexOf(value) !== -1) {
+      return null;
     }
+
+    invertedRobot[value] = key;
+    invertedValues.push(value);
   }
 
   return invertedRobot;
