32e8aa8b5aac16b2b2349f3dae07396e32cecf4b
stunnerboyone <stunnerboy691@gmail.com>
2023-07-28 21:40:39 +0300

    Solution "Situs inversus"

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,18 @@
 
 function inverseRobot(robot) {
   // write code here
+  const keys = Object.keys(robot);
+  const value = Object.values(robot);
+  const fixedRobot = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    if (fixedRobot.hasOwnProperty(value[i])) {
+      return null;
+    }
+    fixedRobot[value[i]] = keys[i];
+  }
+
+  return fixedRobot;
 }
 
 module.exports = inverseRobot;
