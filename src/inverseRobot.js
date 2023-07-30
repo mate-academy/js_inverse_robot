49b845ecafa376cae6394567e76be1b0ccbceabf
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-07-30 18:31:15 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const KEYS = Object.values(robot);
+  const VALUES = Object.keys(robot);
+  const INVERSED_ROBOT = {};
+  const UNIQUE_KEYS = [ ...new Set(KEYS) ];
+
+  if (UNIQUE_KEYS.length !== VALUES.length) {
+    return null;
+  }
+
+  for (let i = 0; i < KEYS.length; i++) {
+    INVERSED_ROBOT[KEYS[i]] = VALUES[i];
+  }
+
+  return INVERSED_ROBOT;
 }
 
 module.exports = inverseRobot;
