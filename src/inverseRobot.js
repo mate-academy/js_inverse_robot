ce3c9dc62a9296fb7b81854d3bb515cf938ddac3
Serhii Lapin <slapin1986@gmail.com>
2023-07-30 16:00:21 +0300

    Inverse_Robot_ SL

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,8 +6,31 @@
  * @return {object}
 */
 
-function inverseRobot(robot) {
-  // write code here
+function inverseRobot (robot) {
+  const INVERSE_ROBOT = {};
+  let COUNT = 0;
+  let COUNT_INVERSE = 0;
+
+  for (const key in robot) {
+    INVERSE_ROBOT[robot[key]] = key;
+    if (robot.hasOwnProperty(key)) {
+      COUNT++;
+    }
+  }
+
+  for (const key in INVERSE_ROBOT) {
+    if (INVERSE_ROBOT.hasOwnProperty(key)) {
+      COUNT_INVERSE++;
+    }
+  }
+
+  if (COUNT !== COUNT_INVERSE) {
+    console.log(null); 
+    return;
+  }
+
+  console.log(INVERSE_ROBOT);
+
 }
 
 module.exports = inverseRobot;
