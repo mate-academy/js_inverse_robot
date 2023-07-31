a0f30d4ae21cfdd0eec1c21a3170f93b6b231d39
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-07-31 11:03:48 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,17 @@
 
 function inverseRobot(robot) {
   // write code here
+  const robotFix = {};
+
+  for (const key in robot) {
+    if (robotFix.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    robotFix[robot[key]] = key;
+  }
+
+  return robotFix;
 }
 
 module.exports = inverseRobot;
