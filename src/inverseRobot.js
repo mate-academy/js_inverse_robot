4a26df8a9ea73436b1c79ce569b31e208e465c30
davinnchii <zaikoivan99@gmail.com>
2023-07-31 14:47:55 +0200

    Add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,27 @@
 
 function inverseRobot(robot) {
   // write code here
+  const reversedRobot = {};
+
+  for (const key in robot) {
+    reversedRobot[robot[key]] = key;
+  }
+
+  // Check our input object for duplicate values
+  const isDuplicates = Object.values(robot).map(function(item, idx, arr) {
+    return arr.indexOf(item) !== idx;
+  });
+
+  // If there are some values which are duplicate
+  // it will have value of true in our isDuplicates array,
+  // so we check it with for loop
+  for (const i of isDuplicates) {
+    if (i) {
+      return null;
+    }
+  }
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
