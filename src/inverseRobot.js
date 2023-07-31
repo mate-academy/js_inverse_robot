e1084ddaf03c265ce029d3a12a93919c114e749b
KlymSudakov <sudakov.klym@gmail.com>
2023-07-31 17:10:40 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,23 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const inversedRobot = {};
+
+  for (const key in robot) {
+    if (robot.hasOwnProperty(key)) {
+      const value = robot[key];
+
+      if (inversedRobot.hasOwnProperty(value)) {
+        return null;
+      }
+      inversedRobot[value] = key;
+    }
+  }
+
+  return inversedRobot;
 }
 
 module.exports = inverseRobot;
