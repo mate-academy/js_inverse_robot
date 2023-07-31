9866a7440aca4220577be196ce8a998ffee4f50d
davinnchii <zaikoivan99@gmail.com>
2023-07-31 19:19:52 +0200

    Changed duplicate detection, according to comments

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -11,21 +11,10 @@ function inverseRobot(robot) {
   const reversedRobot = {};
 
   for (const key in robot) {
-    reversedRobot[robot[key]] = key;
-  }
-
-  // Check our input object for duplicate values
-  const isDuplicates = Object.values(robot).map(function(item, idx, arr) {
-    return arr.indexOf(item) !== idx;
-  });
-
-  // If there are some values which are duplicate
-  // it will have value of true in our isDuplicates array,
-  // so we check it with for loop
-  for (const i of isDuplicates) {
-    if (i) {
+    if (reversedRobot.hasOwnProperty(robot[key])) {
       return null;
     }
+    reversedRobot[robot[key]] = key;
   }
 
   return reversedRobot;
