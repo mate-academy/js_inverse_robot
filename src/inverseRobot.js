382f392ea5e4eea4688fbb879ac214638edc35f2
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-07-31 15:20:19 +0300

    solved task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const REVERSE_ROBOT = {};
+  const VALUES = Object.values(robot);
+  const KEYS = Object.keys(robot);
+  const REMOVE_DUBLICATE_VALUES = new Set(VALUES);
+
+  if ([...REMOVE_DUBLICATE_VALUES].length !== VALUES.length) {
+    return null;
+  }
+
+  for (let i = 0; i < KEYS.length; i++) {
+    REVERSE_ROBOT[VALUES[i]] = KEYS[i];
+  }
+
+  return REVERSE_ROBOT;
 }
 
 module.exports = inverseRobot;
