dc159dadf3d1d2466ecd4b64c2e3451e271ee7e9
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-07-31 12:56:43 +0300

    inverse robot solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,19 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const inverseObject = {};
+
+  for (const key in robot) {
+    const ROBOT_KEYS = robot[key];
+
+    if (ROBOT_KEYS in inverseObject) {
+      return null;
+    }
+
+    inverseObject[ROBOT_KEYS] = key;
+  }
+
+  return inverseObject;
 }
 
 module.exports = inverseRobot;
