52c9b8e12cdfa993f87b018814c2c5073c836686
Yevhenii Voronin <voronine@ukr.net>
2023-07-31 15:58:44 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -2,12 +2,28 @@
 
 /*
  * @param {object} robot
- *
+ *Example:
+```
+ const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
+ const robert = { Robert: 'name', 123: 'chipVer', 113: 'chipVer' };
+ inverseRobot(robert) === null
+ inverseRobot(kolli) === { name: 'Kolli', chipVer: '123', wheels: '3' }
+```
+
  * @return {object}
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const obj = {};
+
+  for (const key in robot) {
+    if (robot[key] in obj) {
+      return null;
+    }
+    Object.assign(obj, { [robot[key]]: key });
+  }
+
+  return obj;
 }
 
 module.exports = inverseRobot;
