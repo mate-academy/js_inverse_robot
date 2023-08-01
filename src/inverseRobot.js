1d0180758f162a5b1b03d6b495833b6d00054632
valet2102 <valikyanyk@gmail.com>
2023-08-01 20:18:41 +0300

    fix comments and fix check properties

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,41 +8,16 @@
 
 function inverseRobot(robot) {
   // write code here
-  const INVERSE_ROBOT = {};
-
-  // #1 use for in
+  const inversRobot = {};
 
   for (const key in robot) {
-    if (INVERSE_ROBOT[robot[key]]) {
+    if (inversRobot.hasOwnProperty(robot[key])) {
       return null;
     }
-    INVERSE_ROBOT[robot[key]] = key;
+    inversRobot[robot[key]] = key;
   }
 
-  // #2 use for of and Object.entries
-  /*
-  const ROBOT_ENTRIES = Object.entries(robot);
-  for (const key of ROBOT_ENTRIES) {
-    if (INVERSE_ROBOT[key[1]]) {
-      return null;
-    }
-
-    INVERSE_ROBOT[key[1]] = key[0];
-  }
-  */
-
-  // #3 use for () and Object.entries
-  /*
-  const ROBOT_ENTRIES = Object.entries(robot);
-  for (let i = 0; i < array.length; i++) {
-    if (newObject[array[i][1]]) {
-      return null;
-    }
-    newObject[array[i][1]] = array[i][0];
-  }
-  */
-
-  return INVERSE_ROBOT;
+  return inversRobot;
 }
 
 module.exports = inverseRobot;
