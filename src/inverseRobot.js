390900514c31e5da94d8a49c10f24435ffbfbe27
Petro Boreiko <boreikopetro4@gmail.com>
2023-08-01 10:51:27 +0200

    added task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,24 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const reversedRobot = {};
+  // const valuesRoobot = Object.values(robot);
+
+  // for (let i = 0; i < valuesRoobot.length; i++) {
+  //   if (valuesRoobot.slice(i + 1).includes(valuesRoobot[i])) {
+  //     return null;
+  //   }
+  // }
+
+  for (const key in robot) {
+    if (reversedRobot.hasOwnProperty(robot[key])) {
+      return null;
+    }
+
+    reversedRobot[robot[key]] = key;
+  }
+
+  return reversedRobot;
 }
 
 module.exports = inverseRobot;
