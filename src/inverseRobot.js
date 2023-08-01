be7a0cd28339d7a8c2ce2d334e6d8d265aa085b4
BeginerAlex <aleksander9677@gmail.com>
2023-08-01 14:02:11 +0200

    task_solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,7 +7,23 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
+  const prevValue = new Set();
+  const copyRobot = {};
+
+  for (const key in robot) {
+    if (robot.hasOwnProperty(key)) {
+      const currentValue = robot[key];
+
+      if (prevValue.has(currentValue)) {
+        return null;
+      }
+
+      copyRobot[currentValue] = key;
+      prevValue.add(currentValue);
+    }
+  }
+
+  return copyRobot;
 }
 
 module.exports = inverseRobot;
