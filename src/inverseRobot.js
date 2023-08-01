d36a1f7973418f21d2c902a325d335e4f9dd298a
valet2102 <valikyanyk@gmail.com>
2023-08-01 09:14:27 +0300

    fix commits

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,17 +8,41 @@
 
 function inverseRobot(robot) {
   // write code here
-  const newObject = {};
-  const array = Object.entries(robot);
+  const INVERSE_ROBOT = {};
 
+  // #1 use for in
+
+  for (const key in robot) {
+    if (INVERSE_ROBOT[robot[key]]) {
+      return null;
+    }
+    INVERSE_ROBOT[robot[key]] = key;
+  }
+
+  // #2 use for of and Object.entries
+  /*
+  const ROBOT_ENTRIES = Object.entries(robot);
+  for (const key of ROBOT_ENTRIES) {
+    if (INVERSE_ROBOT[key[1]]) {
+      return null;
+    }
+
+    INVERSE_ROBOT[key[1]] = key[0];
+  }
+  */
+
+  // #3 use for () and Object.entries
+  /*
+  const ROBOT_ENTRIES = Object.entries(robot);
   for (let i = 0; i < array.length; i++) {
     if (newObject[array[i][1]]) {
       return null;
     }
     newObject[array[i][1]] = array[i][0];
   }
+  */
 
-  return newObject;
+  return INVERSE_ROBOT;
 }
 
 module.exports = inverseRobot;
