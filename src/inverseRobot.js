437e5ab8866079777e0adb8b6affba6d2e65fe24
Taras Voievoda <voevoda.taras@gmail.com>
2023-08-02 10:41:41 +0300

    add key check in loop

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -6,22 +6,22 @@
  * @return {object}
 */
 
-const objLength = (obj) => {
-  return Object.keys(obj).length;
-};
+// const objLength = (obj) => {
+//   return Object.keys(obj).length;
+// };
 
 function inverseRobot(robot) {
   const naturalRobot = {};
 
   for (const key in robot) {
+    if (robot[key] in naturalRobot) {
+      return null;
+    }
+
     naturalRobot[robot[key]] = key;
   }
 
-  if (objLength(naturalRobot) === objLength(robot)) {
-    return naturalRobot;
-  }
-
-  return null;
+  return naturalRobot;
 }
 
 module.exports = inverseRobot;
