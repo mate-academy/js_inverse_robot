ee4fecdd27d0b2c1341c4d1426d3c9b8bd6a6a76
Tamara Chernetska <cty2802@gmail.com>
2023-08-02 15:22:54 +0300

    add task solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -4,10 +4,22 @@
  * @param {object} robot
  *
  * @return {object}
-*/
+ */
 
 function inverseRobot(robot) {
-  // write code here
+  const inversedObject = {};
+
+  for (const key in robot) {
+    const newKey = robot[key];
+
+    if (newKey in inversedObject) {
+      return null;
+    }
+
+    inversedObject[newKey] = key;
+  }
+
+  return inversedObject;
 }
 
 module.exports = inverseRobot;
