e47b930b0a588e2ade2390c6388dec170617cfaf
Sergey <serg9205@gmail.com>
2023-08-03 17:46:20 +0300

    finish the task

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,24 @@
 
 function inverseRobot(robot) {
   // write code here
+
+  const inverseRobotResult = {};
+  const arrayOfKeys = [];
+
+  for (const key in robot) {
+    inverseRobotResult[robot[key]] = key;
+    arrayOfKeys.push(robot[key]);
+  }
+
+  arrayOfKeys.sort();
+
+  for (let i = 0; i < arrayOfKeys.length; i++) {
+    if (arrayOfKeys[i] === arrayOfKeys[i + 1]) {
+      return null;
+    }
+  }
+
+  return inverseRobotResult;
 }
 
 module.exports = inverseRobot;
