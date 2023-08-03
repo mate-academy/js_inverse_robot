8874b2997f308816c6f5a7be03ce6d1cf16e11e2
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-03 15:55:05 +0200

    change loop to for in, use object.hasOwn

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,21 +7,45 @@
 */
 
 function inverseRobot(robot) {
-  const robotValues = Object.values(robot);
-  const robotKeys = Object.keys(robot);
-  const newObj = {};
+  const outRobot = { ...robot };
+  const newRobot = {};
+  const keysArr = [];
+  const valuesArr = [];
 
-  for (const value of robotValues) {
-    if (robotValues.indexOf(value) !== robotValues.lastIndexOf(value)) {
+  for (const key in outRobot) {
+    keysArr.push(key);
+  }
+
+  for (let i = 0; i < keysArr.length; i++) {
+    valuesArr.push(outRobot[keysArr[i]]);
+
+    if (valuesArr.indexOf(valuesArr[i])
+      !== valuesArr.lastIndexOf(valuesArr[i])) {
       return null;
     }
   }
 
-  for (let i = 0; i < robotKeys.length; i++) {
-    newObj[robotValues[i]] = robotKeys[i];
+  for (let i = 0; i < valuesArr.length; i++) {
+    newRobot[valuesArr[i]] = keysArr[i];
   }
 
-  return { ...newObj };
+  return { ...newRobot };
+
+  //   const robotValues = Object.values(robot);
+  //   const robotKeys = Object.keys(robot);
+  //   const newObj = {};
+
+  //   for (const value of robotValues) {
+  //     if (robotValues.indexOf(value) !== robotValues.lastIndexOf(value)) {
+  //       return null;
+  //     }
+  //   }
+
+  //   for (let i = 0; i < robotKeys.length; i++) {
+  //     newObj[robotValues[i]] = robotKeys[i];
+  //   }
+
+  //   return { ...newObj };
 }
 
 module.exports = inverseRobot;
