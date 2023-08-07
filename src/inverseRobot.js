5d069dab8d42bc556a069e8677f3cb9e389d7e5f
Nikita <cool.nikita734@gmail.com>
2023-08-07 16:26:23 +0200

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,23 @@
 
 function inverseRobot(robot) {
   // write code here
+  const invertedRobot = {};
+  const valuesSeen = {};
+
+  for (const key in robot) {
+    if (robot.hasOwnProperty(key)) {
+      const value = robot[key];
+
+      if (valuesSeen[value]) {
+        return null;
+      }
+
+      invertedRobot[value] = key;
+      valuesSeen[value] = true;
+    }
+  }
+
+  return invertedRobot;
 }
 
 module.exports = inverseRobot;
