61bfd3e878ac732819e4f6d23acba79faa9fcee7
YehorBoiku <yehorboiku@gmail.com>
2023-08-17 22:17:22 +0300

    Solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -8,6 +8,19 @@
 
 function inverseRobot(robot) {
   // write code here
+  let repairedRobot = {};
+
+  for (const key in robot) {
+    if (repairedRobot[`${robot[key]}`]) {
+      repairedRobot = null;
+
+      return repairedRobot;
+    }
+
+    repairedRobot[`${robot[key]}`] = key;
+  }
+
+  return repairedRobot;
 }
 
 module.exports = inverseRobot;
