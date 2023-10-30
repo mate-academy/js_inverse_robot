bd5344fed1d75a02486f97187e88511641f324c5
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-10-30 20:40:46 +0100

    solution

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -1,13 +1,28 @@
-'use strict';
-
-/*
- * @param {object} robot
- *
- * @return {object}
-*/
+"use strict";
 
 function inverseRobot(robot) {
-  // write code here
+  const inverse = {};
+
+  for (const key in robot) {
+    const value = robot[key];
+
+    if (inverse[value] !== undefined) {
+      return null;
+    }
+
+    inverse[value] = key;
+  }
+
+  return inverse;
 }
+
+const kolli = { Kolli: "name", 123: "chipVer", 3: "wheels" };
+const robert = { Robert: "name", 123: "chipVer", 113: "chipVer" };
+
+const resultKolli = inverseRobot(kolli);
+const resultRobert = inverseRobot(robert);
+
+console.log(resultKolli); // { name: 'Kolli', chipVer: '123', wheels: '3' }
+console.log(resultRobert); // null
 
 module.exports = inverseRobot;
