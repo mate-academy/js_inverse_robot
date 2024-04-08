66f7d93975b33f90397699770c8beaf6e4eb61f4
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-08 20:18:43 +0300

    solution 6

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,20 +7,12 @@
 */
 
 function inverseRobot(robot) {
-  const KEYS = Object.values(robot);
-  const VALUES = Object.keys(robot);
-  const INVERSED_ROBOT = {};
-  const UNIQUE_KEYS = [ ...new Set(KEYS) ];
+  const newRobot = Object.fromEntries(Object.entries(robot)
+    .map(([key, value]) =>
+      [value, key]));
 
-  if (UNIQUE_KEYS.length !== VALUES.length) {
-    return null;
-  }
-
-  for (let i = 0; i < KEYS.length; i++) {
-    INVERSED_ROBOT[KEYS[i]] = VALUES[i];
-  }
-
-  return INVERSED_ROBOT;
+  return Object.keys(newRobot).length === Object.values(robot).length
+    ? newRobot : null;
 }
 
 module.exports = inverseRobot;
