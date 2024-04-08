412eb2433234ce0597685f4e16c468c5314c0430
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-08 20:45:36 +0300

    solution 7

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,12 +7,20 @@
 */
 
 function inverseRobot(robot) {
-  const newRobot = Object.fromEntries(Object.entries(robot)
-    .map(([key, value]) =>
-      [value, key]));
+  const REVERSE_ROBOT = {};
+  const VALUES = Object.values(robot);
+  const KEYS = Object.keys(robot);
+  const REMOVE_DUBLICATE_VALUES = new Set(VALUES);
 
-  return Object.keys(newRobot).length === Object.values(robot).length
-    ? newRobot : null;
+  if ([...REMOVE_DUBLICATE_VALUES].length !== VALUES.length) {
+    return null;
+  }
+
+  for (let i = 0; i < KEYS.length; i++) {
+    REVERSE_ROBOT[VALUES[i]] = KEYS[i];
+  }
+
+  return REVERSE_ROBOT;
 }
 
 module.exports = inverseRobot;
