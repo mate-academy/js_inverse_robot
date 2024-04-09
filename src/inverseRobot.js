a44140ddc05794b2932d44b764ed7dfa5bb78dcb
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-09 17:20:56 +0300

    solution 9.2

--- src/inverseRobot.js
+++ src/inverseRobot.js
@@ -7,17 +7,25 @@
 */
 
 function inverseRobot(robot) {
-  // write code here
   const inverse = {};
+  const propertyValues = Object.values(robot);
+  let current;
+
+  for (let i = 0; i < propertyValues.length; i++) {
+    current = propertyValues[i];
+
+    for (let j = i + 1; j < propertyValues.length; j++) {
+      if (current === propertyValues[j]) {
+        return null;
+      }
+    }
+  }
 
   for (const key in robot) {
-    if (Object.prototype.hasOwnProperty.call(inverse, robot[key])) {
-      return null;
-    }
-
     inverse[robot[key]] = key;
   }
 
   return inverse;
 }
+
 module.exports = inverseRobot;
